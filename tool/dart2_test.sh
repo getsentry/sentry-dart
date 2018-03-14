1208ab4f597632621b2932f48968b2cd2c34ce77
Anatoly Pulyaevskiy <anatoly.pulyaevskiy@gmail.com>
2018-03-14 15:09:24 -0700

    Run tests with --preview-dart-2 flag

---
+++ tool/dart2_test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Temporary workaround until Pub supports --preview-dart-2 flag
+set -e
+for filename in test/*_test.dart; do
+    dart --preview-dart-2 --enable_asserts "$filename"
+done
