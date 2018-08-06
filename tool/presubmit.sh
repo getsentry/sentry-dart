d84caa4a82c702ae358d1c67cd3d5cdb9a19141a
Lejard Hadrien <hadrien.lejard@gmail.com>
2018-08-06 15:07:14 +0200

    remove dart2_test, use pub run

--- tool/presubmit.sh
+++ tool/presubmit.sh
@@ -4,7 +4,6 @@ set -e
 set -x
 
 pub get
-dartanalyzer --strong --fatal-warnings ./
+dartanalyzer --fatal-warnings ./
 pub run test --platform vm --platform chrome
-./tool/dart2_test.sh
 dartfmt -n --set-exit-if-changed ./
