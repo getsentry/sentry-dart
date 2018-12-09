c81e223823c86b02e3f02eed68630509a4a4e4f5
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2018-12-09 21:19:34 +0700

    fix https://github.com/flutter/flutter/issues/24720 warning from `flutter analyze` deprecated_member_use

--- test/version_test.dart
+++ test/version_test.dart
@@ -12,7 +12,7 @@ void main() {
   group('sdkVersion', () {
     test('matches that of pubspec.yaml', () {
       final dynamic pubspec =
-          yaml.loadYaml(new File('pubspec.yaml').readAsStringSync());
+          yaml.loadYaml(new File('../pubspec.yaml').readAsStringSync());
       expect(sdkVersion, pubspec['version']);
     });
   });
