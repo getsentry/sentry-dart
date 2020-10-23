621d6c42d7f5a34365f6742f69f98082c973c44c
rxlabz <erick@rxlabz.com>
2020-10-23 15:40:37 +0200

    update tests

--- dart/lib/src/client.dart
+++ dart/lib/src/client.dart
@@ -29,7 +29,6 @@ abstract class SentryClient {
     }
   }
 
-  @protected
   @visibleForTesting
   SentryOptions options;
 
--- dart/test/sentry_client_test.dart
+++ dart/test/sentry_client_test.dart
@@ -17,28 +17,28 @@ void main() {
     test('captures event, sample rate is 100% enabled', () {
       options.sampleRate = 1.0;
       final client = SentryClient(options);
-      client.transport = transport;
+      client.options.transport = transport;
       client.captureEvent(fakeEvent);
 
-      verify(client.transport.send(any)).called(1);
+      verify(client.options.transport.send(any)).called(1);
     });
 
     test('do not capture event, sample rate is 0% disabled', () {
       options.sampleRate = 0.0;
       final client = SentryClient(options);
-      client.transport = transport;
+      client.options.transport = transport;
       client.captureEvent(fakeEvent);
 
-      verifyNever(client.transport.send(any));
+      verifyNever(client.options.transport.send(any));
     });
 
     test('do not capture event, sample rate is null', () {
       options.sampleRate = null;
       final client = SentryClient(options);
-      client.transport = transport;
+      client.options.transport = transport;
       client.captureEvent(fakeEvent);
 
-      verifyNever(client.transport.send(any));
+      verifyNever(client.options.transport.send(any));
     });
   });
 }
