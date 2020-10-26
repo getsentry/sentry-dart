eaa39e0e3d39ece28ed9c118650b2a043831a0ed
rxlabz <erick@rxlabz.com>
2020-10-26 22:20:35 +0100

    more applyScope tests

--- dart/test/sentry_client_test.dart
+++ dart/test/sentry_client_test.dart
@@ -124,8 +124,8 @@ void main() {
       final client = SentryClient(options);
       await client.captureEvent(event, scope: scope);
 
-      expect(userId, '123');
-      expect(eventLevel, 'error');
+      expect(userId, user.id);
+      expect(eventLevel, SentryLevel.error.name);
       expect(eventTransaction, transaction);
       expect(eventFingerprint, fingerprint);
       expect(eventBreadcrumbMessage, crumb.message);
@@ -133,6 +133,75 @@ void main() {
       expect(capturedEventTagValue, eventTagValue);
       expect(capturedScopeExtraValue, scopeExtraValue);
       expect(capturedEventExtraValue, eventExtraValue);
+    });
+  });
+
+  group('SentryClient : apply partial scope to the captured event', () {
+    SentryOptions options;
+    String capturedLevel;
+    String capturedTransaction;
+    String capturedBreadcrumbMessage;
+    List<dynamic> capturedFingerprint;
+    String capturedUserId;
+    Scope scope;
+
+    final transaction = '/test/scope';
+    final eventTransaction = '/event/transaction';
+    final fingerprint = ['foo', 'bar', 'baz'];
+    final eventFingerprint = ['123', '456', '798'];
+    final user = User(id: '123');
+    final eventUser = User(id: '987');
+    final crumb = Breadcrumb(message: 'bread');
+    final eventCrumbs = [Breadcrumb(message: 'bread')];
+
+    final event = SentryEvent(
+      level: SentryLevel.warning,
+      transaction: eventTransaction,
+      userContext: eventUser,
+      fingerprint: eventFingerprint,
+      breadcrumbs: eventCrumbs,
+    );
+
+    final mockClient = MockClient((request) async {
+      if (request.method == 'POST') {
+        final body = const JsonDecoder().convert(
+          Utf8Codec().decode(request.bodyBytes),
+        );
+
+        capturedLevel = body['level'];
+        capturedTransaction = body['transaction'];
+        capturedFingerprint = body['fingerprint'];
+        capturedUserId = body['user']['id'];
+        capturedBreadcrumbMessage =
+            body['breadcrumbs']['values'].first['message'];
+
+        return Response('{"id": "test-event-id"}', 200);
+      }
+      fail(
+        'Unexpected request on ${request.method} ${request.url} in HttpMock',
+      );
+    });
+
+    setUp(() {
+      options = SentryOptions(dsn: fakeDsn);
+      options.compressPayload = false;
+      options.httpClient = mockClient;
+      scope = Scope(options)
+        ..user = user
+        ..transaction = transaction
+        ..fingerprint = fingerprint
+        ..addBreadcrumb(crumb);
+    });
+
+    test('should not apply the scope to non null event fields ', () async {
+      final client = SentryClient(options);
+      await client.captureEvent(event, scope: scope);
+
+      expect(capturedUserId, eventUser.id);
+      expect(capturedLevel, SentryLevel.warning.name);
+      expect(capturedTransaction, eventTransaction);
+      expect(capturedFingerprint, eventFingerprint);
+      expect(capturedBreadcrumbMessage, eventCrumbs.first.message);
     });
   });
 
