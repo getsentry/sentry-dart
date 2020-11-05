9772d292a656eb33b71caa268fc6b29581f33355
rxlabz <erick@rxlabz.com>
2020-11-05 14:44:44 +0100

    immutable request

--- dart/lib/src/protocol/request.dart
+++ dart/lib/src/protocol/request.dart
@@ -16,33 +16,42 @@ class Request {
   /// Submitted data in a format that makes the most sense.
   /// SDKs should discard large bodies by default.
   /// Can be given as string or structural data of any format.
-  final dynamic data;
+  final dynamic _data;
+  dynamic get data => _data;
 
   /// The cookie values as string.
   final String cookies;
+
+  final Map<String, String> _headers;
 
   /// A dictionary of submitted headers.
   /// If a header appears multiple times it,
   /// needs to be merged according to the HTTP standard for header merging.
   /// Header names are treated case-insensitively by Sentry.
-  final Map<String, String> headers;
+  Map<String, String> get headers => Map.unmodifiable(_headers);
+
+  final Map<String, String> _env;
 
   /// A dictionary containing environment information passed from the server.
   /// This is where information such as CGI/WSGI/Rack keys go that are not HTTP headers.
-  final Map<String, String> env;
+  Map<String, String> get env => Map.unmodifiable(_env);
 
-  final Map<String, String> other;
+  Map<String, String> get other => Map.unmodifiable(_other);
+  final Map<String, String> _other;
 
-  Request({
+  const Request({
     this.url,
     this.method,
     this.queryString,
-    this.data,
     this.cookies,
-    this.headers,
-    this.env,
-    this.other,
-  });
+    dynamic data,
+    Map<String, String> headers,
+    Map<String, String> env,
+    Map<String, String> other,
+  })  : _data = data,
+        _headers = headers,
+        _env = env,
+        _other = other;
 
   Map<String, dynamic> toJson() {
     final json = <String, dynamic>{};
@@ -81,4 +90,25 @@ class Request {
 
     return json;
   }
+
+  Request copyWith({
+    String url,
+    String method,
+    String queryString,
+    String cookies,
+    dynamic data,
+    Map<String, String> headers,
+    Map<String, String> env,
+    Map<String, String> other,
+  }) =>
+      Request(
+        url: url ?? this.url,
+        method: method ?? this.method,
+        queryString: queryString ?? this.queryString,
+        cookies: cookies ?? this.cookies,
+        data: data ?? _data,
+        headers: headers ?? _headers,
+        env: env ?? _env,
+        other: other ?? _other,
+      );
 }
