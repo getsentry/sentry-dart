57310ef542b77bb4b52be97e202009a1f095a0c1
Manoel Aranda Neto <marandaneto@gmail.com>
2020-11-17 13:26:28 +0100

    fix test

--- flutter/test/sentry_flutter_test.dart
+++ flutter/test/sentry_flutter_test.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:sentry/sentry.dart';
+import 'package:sentry_flutter/default_integrations.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('sentry_flutter');
@@ -7,12 +9,14 @@ void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {});
   });
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
+  });
+
+  test('nativeSdkIntegration wont throw', () async {
+    await nativeSdkIntegration(SentryOptions(), channel);
   });
 }
