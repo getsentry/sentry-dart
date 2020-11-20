bf35dad030a1ed30abfed63b96e98222d9328b57
rxlabz <erick@rxlabz.com>
2020-11-20 10:15:17 +0100

    fix contexts.parsing

--- dart/lib/src/protocol/contexts.dart
+++ dart/lib/src/protocol/contexts.dart
@@ -27,6 +27,22 @@ class Contexts extends MapView<String, dynamic> {
 
   factory Contexts.fromJson(Map<String, dynamic> data) {
     final contexts = Contexts(
+      device: data[Device.type] != null
+          ? Device.fromJson(Map<String, dynamic>.from(data[Device.type]))
+          : null,
+      operatingSystem: data[OperatingSystem.type] != null
+          ? OperatingSystem.fromJson(
+              Map<String, dynamic>.from(data[OperatingSystem.type]))
+          : null,
+      app: data[App.type] != null
+          ? App.fromJson(Map<String, dynamic>.from(data[App.type]))
+          : null,
+      browser: data[Browser.type] != null
+          ? Browser.fromJson(Map<String, dynamic>.from(data[Browser.type]))
+          : null,
+      gpu: data[Gpu.type] != null
+          ? Gpu.fromJson(Map<String, dynamic>.from(data[Gpu.type]))
+          : null,
       runtimes: data[SentryRuntime.type] != null
           ? [
               SentryRuntime.fromJson(
