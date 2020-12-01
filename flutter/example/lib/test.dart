0b4d0d743036ebb792f5a611b852985be7cdcedf
Manoel Aranda Neto <marandaneto@gmail.com>
2020-12-01 11:45:37 +0100

    remove

--- flutter/example/lib/test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:sentry/sentry.dart';
-
-SentryEvent beforeSend(SentryEvent event, {dynamic hint}) {
-  return hint is MyHint ? null : event;
-}
-
-Future<void> main() async {
-  await Sentry.init((options) => options.beforeSend = beforeSend);
-}
