51a4d373e189dad1b7be2f2af6f906febce071d1
Manoel Aranda Neto <marandaneto@gmail.com>
2020-12-07 13:41:23 +0100

    CI: Deactivate pana for sentry_flutter

--- dart/tool/presubmit.sh
+++ dart/tool/presubmit.sh
@@ -3,7 +3,15 @@
 set -e
 set -x
 
+# get current package's dependencies
 pub get
+# static code analyzer
 dartanalyzer --fatal-infos --fatal-warnings ./
+# tests
 pub run test -p "chrome,vm"
+# formatting
 dartfmt -n --set-exit-if-changed ./
+# pub score
+pana
+# dry publish
+pub publish --dry-run
