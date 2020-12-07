8e5dc894862de5fc0e369bb998d9b3ad83b0b0ae
Manoel Aranda Neto <5731772+marandaneto@users.noreply.github.com>
2020-12-07 13:49:55 +0100

    CI: Deactivate pana for sentry_flutter (#235)

--- flutter/tool/presubmit.sh
+++ flutter/tool/presubmit.sh
@@ -3,7 +3,15 @@
 set -e
 set -x
 
+# get current package's dependencies
 flutter pub get
+# static code analyzer
 flutter analyze
+# tests
 flutter test
+# formatting
 flutter format -n --set-exit-if-changed ./
+# pub score
+pana
+# dry publish
+pub publish --dry-run
