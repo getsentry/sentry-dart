bc50b7ac058b418c1094eb06b018e67c9f581b60
Manoel Aranda Neto <marandaneto@gmail.com>
2021-02-26 11:10:36 +0100

    fix comment

--- flutter/lib/src/sentry_flutter_options.dart
+++ flutter/lib/src/sentry_flutter_options.dart
@@ -21,7 +21,10 @@ class SentryFlutterOptions extends SentryOptions {
 
   /// Enable or disable the Crash handling on the Native SDKs, e.g.,
   /// UncaughtExceptionHandler and [anrEnabled] for Android.
-  /// SentryCrashIntegration (KSCrash) for iOS.
+  ///
+  /// SentryCrashIntegration (KSCrash) for iOS, turning this feature off on iOS
+  /// has a side effect which is missing the Device's context, e.g.,
+  /// App, Device and Operation System.
   ///
   /// Disabling this feature would affect the [enableAutoSessionTracking]
   /// feature, as this is required to mark Sessions as Crashed.
