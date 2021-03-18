9102c8f86543297e9a98100cb8c51a367ad56316
Manoel Aranda Neto <5731772+marandaneto@users.noreply.github.com>
2021-03-18 13:40:32 +0100

    Update dart/example/main.dart

--- dart/example/main.dart
+++ dart/example/main.dart
@@ -48,7 +48,7 @@ Future<void> runApp() async {
         id: '800',
         username: 'first-user',
         email: 'first@user.lan',
-        // ipAddress: '127.0.0.1',
+        // ipAddress: '127.0.0.1', sendDefaultPii feature is enabled
         extras: <String, String>{'first-sign-in': '2020-01-01'},
       )
       // ..fingerprint = ['example-dart']
