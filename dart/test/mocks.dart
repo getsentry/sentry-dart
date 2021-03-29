c6d3b35fd6b5c00605db97b45a1aa450f905064f
denrase <denrase@gmail.com>
2021-03-29 14:21:15 +0200

    No generated mock for cdtp

--- dart/test/mocks.dart
+++ dart/test/mocks.dart
@@ -1,9 +1,6 @@
-import 'package:mockito/annotations.dart';
 import 'package:sentry/sentry.dart';
 import 'package:sentry/src/protocol.dart';
-import 'package:sentry/src/current_date_provider.dart';
 
-@GenerateMocks([CurrentDateTimeProvider])
 void main() {}
 
 final fakeDsn = 'https://abc@def.ingest.sentry.io/1234567';
