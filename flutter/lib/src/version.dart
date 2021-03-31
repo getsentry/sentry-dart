132c78b135f0bcb036c275006ef264e8f42feae4
Manoel Aranda Neto <marandaneto@gmail.com>
2021-03-31 10:50:18 +0200

    prepare: 5.0.1

--- flutter/lib/src/version.dart
+++ flutter/lib/src/version.dart
@@ -1,5 +1,5 @@
 /// The SDK version reported to Sentry.io in the submitted events.
-const String sdkVersion = '5.0.0';
+const String sdkVersion = '5.0.1';
 
 /// The default SDK name reported to Sentry.io in the submitted events.
 const String sdkName = 'sentry.dart.flutter';
