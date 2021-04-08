c8588143b8075ad82582a8ee119aa81b2597e0ae
Bruno Garcia <bruno@brunogarcia.com>
2021-04-07 21:51:29 -0400

    windows support

---
+++ flutter/example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <sentry_flutter/sentry_flutter_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  SentryFlutterPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("SentryFlutterPlugin"));
+}
