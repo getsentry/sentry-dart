4d15b2c23bb0d0b861257362eaccf1e0c573c8d6
Jonas Uekötter <dev@uekoetter.dev>
2021-04-12 07:56:41 +0200

    improve macos example

--- flutter/example/macos/Runner/MainFlutterWindow.swift
+++ flutter/example/macos/Runner/MainFlutterWindow.swift
@@ -1,7 +1,10 @@
 import Cocoa
 import FlutterMacOS
+import Sentry
 
 class MainFlutterWindow: NSWindow {
+  private let _channel = "example.flutter.sentry.io"
+
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController.init()
     let windowFrame = self.frame
@@ -10,6 +13,29 @@ class MainFlutterWindow: NSWindow {
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
+    let controller = self.contentViewController as! FlutterViewController;
+    let channel = FlutterMethodChannel(name: _channel,
+                                    binaryMessenger: controller.engine.binaryMessenger)
+    channel.setMethodCallHandler(handleMessage)
+
     super.awakeFromNib()
   }
+
+    private func handleMessage(call: FlutterMethodCall, result: FlutterResult) {
+        if call.method == "fatalError" {
+            fatalError("fatalError")
+        } else if call.method == "crash" {
+            SentrySDK.crash()
+        } else if call.method == "capture" {
+            let exception = NSException(
+                name: NSExceptionName("NSException"),
+                reason: "Swift NSException Captured",
+                userInfo: ["details": "lots"])
+            SentrySDK.capture(exception: exception)
+        } else if call.method == "capture_message" {
+            SentrySDK.capture(message: "A message from Swift.")
+        } else if call.method == "throw" {
+            Buggy.throw()
+        }
+    }
 }
