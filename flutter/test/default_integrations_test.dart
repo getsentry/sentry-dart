f6631ebf39dcaf0a2e909c5c217f77a6fbdc9aec
Jonas Uekötter <dev@uekoetter.dev>
2021-04-12 11:37:28 +0200

    Use TestFixture

--- flutter/test/default_integrations_test.dart
+++ flutter/test/default_integrations_test.dart
@@ -227,69 +227,61 @@ void main() {
   });
 
   group('$LoadReleaseIntegration', () {
-    Future<PackageInfo> loadRelease() {
-      return Future.value(PackageInfo(
-        appName: 'sentry_flutter',
-        packageName: 'foo.bar',
-        version: '1.2.3',
-        buildNumber: '789',
-      ));
-    }
+    late LoadReleaseIntegrationFixture fixture;
+
+    setUp(() {
+      fixture = LoadReleaseIntegrationFixture();
+    });
 
     test('does not overwrite options', () async {
-      final options = SentryFlutterOptions(dsn: fakeDsn);
-      options.release = '1.0.0';
-      options.dist = 'dist';
+      fixture.options.release = '1.0.0';
+      fixture.options.dist = 'dist';
 
-      final integration = LoadReleaseIntegration(loadRelease);
-      await integration.call(MockHub(), options);
+      await fixture.getIntegration().call(MockHub(), fixture.options);
 
-      expect(options.release, '1.0.0');
-      expect(options.dist, 'dist');
+      expect(fixture.options.release, '1.0.0');
+      expect(fixture.options.dist, 'dist');
     });
 
     test('sets release and dist if not set on options', () async {
-      final options = SentryFlutterOptions(dsn: fakeDsn);
+      await fixture.getIntegration().call(MockHub(), fixture.options);
 
-      final integration = LoadReleaseIntegration(loadRelease);
-      await integration.call(MockHub(), options);
-
-      expect(options.release, 'foo.bar@1.2.3+789');
-      expect(options.dist, '789');
+      expect(fixture.options.release, 'foo.bar@1.2.3+789');
+      expect(fixture.options.dist, '789');
     });
 
     test('sets app name as in release if packagename is empty', () async {
-      final options = SentryFlutterOptions(dsn: fakeDsn);
-
-      final integration = LoadReleaseIntegration(() {
+      final loader = () {
         return Future.value(PackageInfo(
           appName: 'sentry_flutter',
           packageName: '',
           version: '1.2.3',
           buildNumber: '789',
         ));
-      });
-      await integration.call(MockHub(), options);
+      };
+      await fixture
+          .getIntegration(loader: loader)
+          .call(MockHub(), fixture.options);
 
-      expect(options.release, 'sentry_flutter@1.2.3+789');
-      expect(options.dist, '789');
+      expect(fixture.options.release, 'sentry_flutter@1.2.3+789');
+      expect(fixture.options.dist, '789');
     });
 
     test('release name does not contain ivalid chars', () async {
-      final options = SentryFlutterOptions(dsn: fakeDsn);
-
-      final integration = LoadReleaseIntegration(() {
+      final loader = () {
         return Future.value(PackageInfo(
           appName: '\\/sentry\tflutter \r\nfoo\nbar\r',
           packageName: '',
           version: '1.2.3',
           buildNumber: '789',
         ));
-      });
-      await integration.call(MockHub(), options);
+      };
+      await fixture
+          .getIntegration(loader: loader)
+          .call(MockHub(), fixture.options);
 
-      expect(options.release, '__sentry_flutter _foo_bar_@1.2.3+789');
-      expect(options.dist, '789');
+      expect(fixture.options.release, '__sentry_flutter _foo_bar_@1.2.3+789');
+      expect(fixture.options.dist, '789');
     });
   });
 }
@@ -297,4 +289,22 @@ void main() {
 class Fixture {
   final hub = MockHub();
   final options = SentryFlutterOptions();
+}
+
+class LoadReleaseIntegrationFixture {
+  final hub = MockHub();
+  final options = SentryFlutterOptions(dsn: fakeDsn);
+
+  LoadReleaseIntegration getIntegration({PackageLoader? loader}) {
+    return LoadReleaseIntegration(loader ?? loadRelease);
+  }
+
+  Future<PackageInfo> loadRelease() {
+    return Future.value(PackageInfo(
+      appName: 'sentry_flutter',
+      packageName: 'foo.bar',
+      version: '1.2.3',
+      buildNumber: '789',
+    ));
+  }
 }
