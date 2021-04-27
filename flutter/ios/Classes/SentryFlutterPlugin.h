0772bee186c050452f1beadb26d8abad8c95d63a
Jonas Uekötter <dev@uekoetter.dev>
2021-04-27 16:30:34 +0200

    WIP

--- flutter/ios/Classes/SentryFlutterPlugin.h
+++ flutter/ios/Classes/SentryFlutterPlugin.h
@@ -1,4 +1,8 @@
-#import <Flutter/Flutter.h>
+#if TARGET_OS_IPHONE
+    #import <Flutter/Flutter.h>
+#else
+    #import <FlutterMacOS/FlutterMacOS.h>
+#endif
 
 @interface SentryFlutterPlugin : NSObject<FlutterPlugin>
 @end
