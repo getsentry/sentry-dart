009138cca3b0dfb2e2d2ec8b9f8bf766efea3720
denrase <denrase@gmail.com>
2021-05-17 09:34:05 +0200

    Use for loops and contunie early

--- dart/lib/src/transport/rate_limit_parser.dart
+++ dart/lib/src/transport/rate_limit_parser.dart
@@ -14,33 +14,31 @@ class RateLimitParser {
     if (rateLimitHeader == null) {
       return [];
     }
-
     final rateLimits = <RateLimit>[];
-
     final rateLimitValues = rateLimitHeader.toLowerCase().split(',');
-    rateLimitValues.forEach((rateLimitValue) {
+    for (final rateLimitValue in rateLimitValues) {
       final durationAndCategories = rateLimitValue.trim().split(':');
-
-      if (durationAndCategories.isNotEmpty) {
-        final duration = _parseRetryAfterOrDefault(durationAndCategories[0]);
-
-        if (durationAndCategories.length > 1) {
-          final allCategories = durationAndCategories[1];
-          if (allCategories.isNotEmpty) {
-            final categoryValues = allCategories.split(';');
-            categoryValues.forEach((categoryValue) {
-              final category =
-                  RateLimitCategoryExtension.fromStringValue(categoryValue);
-              if (category != RateLimitCategory.unknown) {
-                rateLimits.add(RateLimit(category, duration));
-              }
-            });
-          } else {
-            rateLimits.add(RateLimit(RateLimitCategory.all, duration));
+      if (durationAndCategories.isEmpty) {
+        continue;
+      }
+      final duration = _parseRetryAfterOrDefault(durationAndCategories[0]);
+      if (durationAndCategories.length <= 1) {
+        continue;
+      }
+      final allCategories = durationAndCategories[1];
+      if (allCategories.isNotEmpty) {
+        final categoryValues = allCategories.split(';');
+        for (final categoryValue in categoryValues) {
+          final category =
+            RateLimitCategoryExtension.fromStringValue(categoryValue);
+          if (category != RateLimitCategory.unknown) {
+            rateLimits.add(RateLimit(category, duration));
           }
         }
+      } else {
+        rateLimits.add(RateLimit(RateLimitCategory.all, duration));
       }
-    });
+    }
     return rateLimits;
   }
 
