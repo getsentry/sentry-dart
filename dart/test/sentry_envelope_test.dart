bffc2efff9100a8f54990cc09d9aa6fb508fcbd7
denrase <denrase@gmail.com>
2021-05-18 18:03:10 +0200

    Add test for binary envelope

--- dart/test/sentry_envelope_test.dart
+++ dart/test/sentry_envelope_test.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:sentry/sentry.dart';
 import 'package:sentry/src/sentry_envelope.dart';
@@ -69,6 +70,38 @@ void main() {
           .forEach(expectedItem.addAll);
 
       expect(actualItem, expectedItem);
+    });
+
+    test('item with binary payload', () async {
+      // Attachment
+
+      final length = () async {
+        return 3535;
+      };
+      final dataFactory = () async {
+        final file = File('test_resources/sentry.png');
+        final bytes = await file.readAsBytes();
+        return bytes;
+      };
+      final attachmentHeader = SentryEnvelopeItemHeader('attachment', length,
+          contentType: 'image/png', fileName: 'sentry.png');
+      final attachmentItem = SentryEnvelopeItem(attachmentHeader, dataFactory);
+
+      // Envelope
+
+      final eventId = SentryId.fromId('3b382f22ee67491f80f7dee18016a7b1');
+      final sdkVersion = SdkVersion(name: 'test', version: 'version');
+      final header = SentryEnvelopeHeader(eventId, sdkVersion);
+      final envelope = SentryEnvelope(header, [attachmentItem]);
+
+      final envelopeData = <int>[];
+      await envelope.envelopeStream().forEach(envelopeData.addAll);
+
+      final expectedEnvelopeFile =
+          File('test_resources/envelope-with-image.envelope');
+      final expectedEnvelopeData = await expectedEnvelopeFile.readAsBytes();
+
+      expect(expectedEnvelopeData, envelopeData);
     });
   });
 }
