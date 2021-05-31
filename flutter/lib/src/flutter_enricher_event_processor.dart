3724d2eb64e0906e2cb52f3e5c2513c54a55c36c
Jonas Uekötter <dev@uekoetter.dev>
2021-05-31 14:25:59 +0200

    flutter format :)

--- flutter/lib/src/flutter_enricher_event_processor.dart
+++ flutter/lib/src/flutter_enricher_event_processor.dart
@@ -81,7 +81,7 @@ class FlutterEnricherEventProcessor {
       // The license registry has a list of licenses entries (MIT, Apache...).
       // Each license entry has a list of packages which licensed under this particular license.
       // Libraries can be dual licensed.
-      // 
+      //
       // We don't care about those license issues, we just want each package name once.
       // Therefore we add each name to a set to make sure we only add it once.
       await LicenseRegistry.licenses.forEach(
