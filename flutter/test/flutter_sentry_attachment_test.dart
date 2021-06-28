0e415032b336847b40c73041762478ebcc121fcd
Jonas Uekötter <dev@uekoetter.dev>
2021-06-28 19:55:58 +0200

    test

--- flutter/test/flutter_sentry_attachment_test.dart
+++ flutter/test/flutter_sentry_attachment_test.dart
@@ -1,1 +1,33 @@
-void main() {}
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+
+void main() {
+  test('fromAsset', () async {
+    final attachment = FlutterSentryAttachment.fromAsset(
+      'foobar.txt',
+      bundle: TestAssetBundle(),
+    );
+
+    expect(attachment.attachmentType, AttachmentType.attachment);
+    expect(attachment.contentType, isNull);
+    expect(attachment.filename, 'foobar.txt');
+    await expectLater(await attachment.bytes, [102, 111, 111, 32, 98, 97, 114]);
+  });
+}
+
+class TestAssetBundle extends CachingAssetBundle {
+  @override
+  Future<ByteData> load(String key) async {
+    if (key == 'foobar.txt') {
+      return ByteData.view(
+        Uint8List.fromList(utf8.encode('foo bar')).buffer,
+      );
+    }
+
+    return ByteData(0);
+  }
+}
