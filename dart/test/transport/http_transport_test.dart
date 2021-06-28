75a7b604d4de9c4f961d54b6287fff1d99a00421
Jonas Uekötter <dev@uekoetter.dev>
2021-06-28 18:45:49 +0200

    fix test

--- dart/test/transport/http_transport_test.dart
+++ dart/test/transport/http_transport_test.dart
@@ -92,7 +92,7 @@ void main() {
           SentryEnvelope.fromEvent(sentryEvent, fixture.options.sdk);
       final eventId = await sut.send(envelope);
 
-      expect(eventId, SentryId.empty());
+      expect(eventId.toString(), SentryId.empty().toString());
       expect(httpCalled, false);
     });
   });
