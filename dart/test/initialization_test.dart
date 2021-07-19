e864b56182e982f28d467dffdc1a900d21920da5
Jonas Uekötter <dev@uekoetter.dev>
2021-07-19 08:02:54 +0200

    Move test

---
+++ dart/test/initialization_test.dart
@@ -0,0 +1,35 @@
+import 'package:sentry/sentry.dart';
+import 'package:test/test.dart';
+
+import 'mocks.dart';
+
+// Tests for the following issue
+// https://github.com/getsentry/sentry-dart/issues/508
+// There are no asserts, test are succesfull if no exceptions are thrown.
+void main() {
+  test('async re-initilization', () async {
+    await Sentry.init((options) {
+      options.dsn = fakeDsn;
+    });
+
+    await Sentry.close();
+
+    await Sentry.init((options) {
+      options.dsn = fakeDsn;
+    });
+  });
+
+  // This is the failure from
+  // https://github.com/getsentry/sentry-dart/issues/508
+  test('re-initilization', () {
+    Sentry.init((options) {
+      options.dsn = fakeDsn;
+    });
+
+    Sentry.close();
+
+    Sentry.init((options) {
+      options.dsn = fakeDsn;
+    });
+  });
+}
