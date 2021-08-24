28c407ee26420a2f4a83625943c32ed812d3c9a8
Jonas Uekötter <dev@uekoetter.dev>
2021-08-24 10:14:35 +0200

    io extends web

--- dart/lib/src/environment/_io_environment_variables.dart
+++ dart/lib/src/environment/_io_environment_variables.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import '_web_environment_variables.dart';
 import 'environment_variables.dart';
 import 'keys.dart';
 
@@ -7,36 +8,17 @@ final EnvironmentVariables envs = IoEnvironmentVariables();
 
 /// In addition to dart defines this io implementation can read from the
 /// environment variables.
-class IoEnvironmentVariables extends EnvironmentVariables {
+class IoEnvironmentVariables extends WebEnvironmentVariables {
   @override
-  String? get environment {
-    if (const bool.hasEnvironment(sentryEnvironment)) {
-      return const String.fromEnvironment(sentryEnvironment);
-    }
-    return Platform.environment[sentryEnvironment];
-  }
+  String? get environment =>
+      super.environment ?? Platform.environment[sentryEnvironment];
 
   @override
-  String? get dsn {
-    if (const bool.hasEnvironment(sentryDsn)) {
-      return const String.fromEnvironment(sentryDsn);
-    }
-    return Platform.environment[sentryDsn];
-  }
+  String? get dsn => super.dsn ?? Platform.environment[sentryDsn];
 
   @override
-  String? get release {
-    if (const bool.hasEnvironment(sentryRelease)) {
-      return const String.fromEnvironment(sentryRelease);
-    }
-    return Platform.environment[sentryRelease];
-  }
+  String? get release => super.release ?? Platform.environment[sentryRelease];
 
   @override
-  String? get dist {
-    if (const bool.hasEnvironment(sentryDist)) {
-      return const String.fromEnvironment(sentryDist);
-    }
-    return Platform.environment[sentryDist];
-  }
+  String? get dist => super.dist ?? Platform.environment[sentryDist];
 }
