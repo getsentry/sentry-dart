66c974ae905de4b7fe4530f3ad41d7840a141a39
Jonas Uekötter <ueman@users.noreply.github.com>
2021-11-23 10:35:26 +0100

    Update sentry_logging/lib/src/version.dart
    Co-authored-by: Manoel Aranda Neto <5731772+marandaneto@users.noreply.github.com>

--- sentry_logging/lib/src/version.dart
+++ sentry_logging/lib/src/version.dart
@@ -2,4 +2,4 @@
 const String sdkVersion = '6.2.0';
 
 /// The default SDK name reported to Sentry.io in the submitted events.
-const String sdkName = 'sentry.dart.sentry_logging';
+const String sdkName = 'sentry.dart.logging';
