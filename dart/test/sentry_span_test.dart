c003afc1e830a7305063ec0213efc10d9c862d6c
Denis Andrašec <denis@Deniss-MacBook-Pro.local>
2021-12-01 17:30:06 +0100

    add missing tests for callback on finish

--- dart/test/sentry_span_test.dart
+++ dart/test/sentry_span_test.dart
@@ -123,6 +123,17 @@ void main() {
     expect(sut.status, SpanStatus.ok());
     expect(sut.finished, true);
   });
+
+  test('callback called on finish',  () async {
+    final sut = fixture.getSut();
+    var numberOfCallbackCalls = 0;
+    sut.finishedCallback = () {
+      numberOfCallbackCalls += 1;
+    };
+    await sut.finish();
+
+    expect(numberOfCallbackCalls, 1);
+  });
 }
 
 class Fixture {
