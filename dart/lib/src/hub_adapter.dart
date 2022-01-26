032d9c845b1eda565997777a5b93fb02a3c0e2f6
Denis Andrašec <denis@Deniss-MacBook-Pro.local>
2022-01-26 15:28:56 +0100

    remove todo

--- dart/lib/src/hub_adapter.dart
+++ dart/lib/src/hub_adapter.dart
@@ -15,7 +15,6 @@ class HubAdapter implements Hub {
   static final HubAdapter _instance = HubAdapter._();
 
   @override
-  // TODO: implement options
   SentryOptions get options => Sentry.currentHub.options;
 
   factory HubAdapter() {
