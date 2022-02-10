f8ee5d095cc17b0b9b88395f88460932e649c16a
Jonas Uekötter <dev@uekoetter.dev>
2022-02-10 19:00:13 +0100

    add test

--- dio/test/sentry_dio_extension_test.dart
+++ dio/test/sentry_dio_extension_test.dart
@@ -1,18 +1,53 @@
 import 'package:dio/dio.dart';
+import 'package:sentry_dio/sentry_dio.dart';
 import 'package:sentry_dio/src/sentry_dio_client_adapter.dart';
 import 'package:sentry_dio/src/sentry_dio_extension.dart';
 import 'package:sentry_dio/src/sentry_transformer.dart';
 import 'package:test/test.dart';
 
-// todo: figure out a way to test if parameter are passed through correctly
+import 'mocks/mock_hub.dart';
 
 void main() {
   group('SentryDioExtension', () {
-    test('addSentry add client and transformer', () {
+    test('addSentry adds $SentryTransformer', () {
       final dio = Dio();
+
       dio.addSentry();
-      expect(dio.httpClientAdapter, isA<SentryDioClientAdapter>());
+
       expect(dio.transformer, isA<SentryTransformer>());
+    });
+
+    test('addSentry adds $SentryDioClientAdapter', () {
+      final dio = Dio();
+
+      dio.addSentry();
+
+      expect(dio.httpClientAdapter, isA<SentryDioClientAdapter>());
+    });
+
+    test('addSentry adds $DioEventProcessor', () {
+      final dio = Dio();
+      final hub = MockHub();
+
+      dio.addSentry(hub: hub);
+
+      expect(
+        hub.options.eventProcessors.whereType<DioEventProcessor>().length,
+        1,
+      );
+    });
+
+    test('addSentry only adds one $DioEventProcessor', () {
+      final dio = Dio();
+      final hub = MockHub();
+
+      dio.addSentry(hub: hub);
+      dio.addSentry(hub: hub);
+
+      expect(
+        hub.options.eventProcessors.whereType<DioEventProcessor>().length,
+        1,
+      );
     });
   });
 }
