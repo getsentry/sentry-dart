e05deb0529afc0422754be487ae30ea53d886425
Denis Andrašec <denis@Deniss-MacBook-Pro.local>
2022-03-09 17:31:15 +0100

    add commnets

--- flutter/lib/src/sentry_native_state.dart
+++ flutter/lib/src/sentry_native_state.dart
@@ -27,10 +27,12 @@ class SentryNativeState {
 
   final _nativeFramesByTraceId = <SentryId, NativeFrames>{};
 
+  /// Adds native frames by trace id.
   void addNativeFrames(SentryId traceId, NativeFrames nativeFrames) {
     _nativeFramesByTraceId[traceId] = nativeFrames;
   }
 
+  /// Returns and removes native frames by trace id.
   NativeFrames? removeNativeFrames(SentryId traceId) {
     return _nativeFramesByTraceId.remove(traceId);
   }
