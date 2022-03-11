2309ac609148f4323c28e04fd1415141f119ce45
Denis Andrašec <denis@Deniss-MacBook-Pro.local>
2022-03-11 18:37:37 +0100

    use toUTC() for app start end

--- flutter/lib/src/integrations/mobile_vitals_integration.dart
+++ flutter/lib/src/integrations/mobile_vitals_integration.dart
@@ -27,7 +27,7 @@ class MobileVitalsIntegration extends Integration<SentryFlutterOptions> {
             'Scheduler binding is null. Can\'t auto detect app start time.');
       } else {
         schedulerBinding.addPostFrameCallback((timeStamp) {
-          _nativeState.appStartEnd = DateTime.now();
+          _nativeState.appStartEnd = DateTime.now().toUtc();
         });
       }
     }
