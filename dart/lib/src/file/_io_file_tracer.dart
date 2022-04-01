b40e292cc88ff72f8d3ffdc9fe583f2e7d4edc99
Jonas Uekötter <dev@uekoetter.dev>
2022-04-01 16:12:46 +0200

    remove duplicated code

--- dart/lib/src/file/_io_file_tracer.dart
+++ dart/lib/src/file/_io_file_tracer.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import '../hub.dart';
 import '../sentry.dart';
 import '../sentry_options.dart';
-import 'sentry_file.dart';
+import 'sentry_io_overrides.dart';
 
 FutureOr<void> runWithFileOverrides(
   AppRunner runner,
@@ -15,20 +15,4 @@ FutureOr<void> runWithFileOverrides(
     runner,
     SentryIoOverrides(hub, options),
   );
-}
-
-class SentryIoOverrides extends IOOverrides {
-  final Hub _hub;
-  final SentryOptions _options;
-
-  SentryIoOverrides(this._hub, this._options);
-
-  @override
-  File createFile(String path) {
-    return SentryFile(
-      super.createFile(path),
-      _hub,
-      _options,
-    );
-  }
 }
