5816177f1f4e8b75482b4c6dad909b0daffa8f6f
Denis Andrašec <denis@Deniss-MBP.home>
2022-04-19 14:31:55 +0200

    Dont set conditionally

--- flutter/android/src/main/kotlin/io/sentry/flutter/SentryFlutterPlugin.kt
+++ flutter/android/src/main/kotlin/io/sentry/flutter/SentryFlutterPlugin.kt
@@ -152,9 +152,7 @@ class SentryFlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
 
       // TODO Set sendClientReports when available on Android
 //      args.getIfNotNull<Boolean>("sendClientReports") { sendClientReports ->
-//        if (sendClientReports) {
-//          options.sendClientReports = sendClientReports
-//        }
+//        options.sendClientReports = sendClientReports
 //      }
 
       options.setBeforeSend { event, _ ->
