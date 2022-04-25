9c73b7e258fe0b951bd310e8199eee6da0cd3b21
Denis Andrašec <denrase@gmail.com>
2022-04-25 11:25:24 +0200

    Client Reports: Bump and Pass Options (Android) (#846)

--- flutter/android/src/main/kotlin/io/sentry/flutter/SentryFlutterPlugin.kt
+++ flutter/android/src/main/kotlin/io/sentry/flutter/SentryFlutterPlugin.kt
@@ -136,7 +136,7 @@ class SentryFlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
       val nativeCrashHandling = (args["enableNativeCrashHandling"] as? Boolean) ?: true
       // nativeCrashHandling has priority over anrEnabled
       if (!nativeCrashHandling) {
-        options.enableUncaughtExceptionHandler = false
+        options.setEnableUncaughtExceptionHandler(false)
         options.isAnrEnabled = false
         // if split symbols are enabled, we need Ndk integration so we can't really offer the option
         // to turn it off
@@ -150,10 +150,7 @@ class SentryFlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
         }
       }
 
-      // TODO Set sendClientReports when available on Android
-//      args.getIfNotNull<Boolean>("sendClientReports") { sendClientReports ->
-//        options.sendClientReports = sendClientReports
-//      }
+      args.getIfNotNull<Boolean>("sendClientReports") { options.setSendClientReports(it) }
 
       options.setBeforeSend { event, _ ->
         setEventOriginTag(event)
