74d591972c8f192a95520cdba5783eaa65ec5942
Jonas Uekötter <dev@uekoetter.dev>
2022-05-05 21:03:18 +0200

    fix order of annotation

--- flutter/test/android_platform_exception_event_processor_test.dart
+++ flutter/test/android_platform_exception_event_processor_test.dart
@@ -1,6 +1,5 @@
-// ignore_for_file: invalid_use_of_internal_member
-
 @TestOn('vm')
+// ignore_for_file: invalid_use_of_internal_member
 
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
