667b8181d5785503600102674488e64158e78b1b
Denis Andrašec <denis@Deniss-MBP.home>
2022-05-17 14:37:31 +0200

    add tests for native channel

--- flutter/test/sentry_native_channel_test.dart
+++ flutter/test/sentry_native_channel_test.dart
@@ -59,6 +59,111 @@ void main() {
       expect(actual?.slowFrames, 2);
       expect(actual?.frozenFrames, 1);
     });
+
+    test('setUser', () async {
+      when(fixture.methodChannel.invokeMethod('setUser', {'user': null}))
+          .thenAnswer((_) => Future.value());
+
+      final sut = fixture.getSut();
+      await sut.setUser(null);
+
+      verify(fixture.methodChannel.invokeMethod('setUser', {'user': null}));
+    });
+
+    test('addBreadcrumb', () async {
+      final breadcrumb = Breadcrumb();
+      when(fixture.methodChannel.invokeMethod(
+              'addBreadcrumb', {'breadcrumb': breadcrumb.toJson()}))
+          .thenAnswer((_) => Future.value());
+
+      final sut = fixture.getSut();
+      await sut.addBreadcrumb(breadcrumb);
+
+      verify(fixture.methodChannel
+          .invokeMethod('addBreadcrumb', {'breadcrumb': breadcrumb.toJson()}));
+    });
+
+    test('clearBreadcrumbs', () async {
+      when(fixture.methodChannel.invokeMethod('clearBreadcrumbs'))
+          .thenAnswer((_) => Future.value());
+
+      final sut = fixture.getSut();
+      await sut.clearBreadcrumbs();
+
+      verify(fixture.methodChannel.invokeMethod('clearBreadcrumbs'));
+    });
+
+    test('setContexts', () async {
+      when(fixture.methodChannel.invokeMethod(
+              'setContexts', {'key': 'fixture-key', 'value': 'fixture-value'}))
+          .thenAnswer((_) => Future.value());
+
+      final sut = fixture.getSut();
+      await sut.setContexts('fixture-key', 'fixture-value');
+
+      verify(fixture.methodChannel.invokeMethod(
+          'setContexts', {'key': 'fixture-key', 'value': 'fixture-value'}));
+    });
+
+    test('removeContexts', () async {
+      when(fixture.methodChannel
+              .invokeMethod('removeContexts', {'key': 'fixture-key'}))
+          .thenAnswer((_) => Future.value());
+
+      final sut = fixture.getSut();
+      await sut.removeContexts('fixture-key');
+
+      verify(fixture.methodChannel
+          .invokeMethod('removeContexts', {'key': 'fixture-key'}));
+    });
+
+    test('setExtra', () async {
+      when(fixture.methodChannel.invokeMethod(
+              'setExtra', {'key': 'fixture-key', 'value': 'fixture-value'}))
+          .thenAnswer((_) => Future.value());
+
+      final sut = fixture.getSut();
+      await sut.setExtra('fixture-key', 'fixture-value');
+
+      verify(fixture.methodChannel.invokeMethod(
+          'setExtra', {'key': 'fixture-key', 'value': 'fixture-value'}));
+    });
+
+    test('removeExtra', () async {
+      when(fixture.methodChannel
+              .invokeMethod('removeExtra', {'key': 'fixture-key'}))
+          .thenAnswer((_) => Future.value());
+
+      final sut = fixture.getSut();
+      await sut.removeExtra('fixture-key');
+
+      verify(fixture.methodChannel
+          .invokeMethod('removeExtra', {'key': 'fixture-key'}));
+    });
+
+    test('setTag', () async {
+      when(fixture.methodChannel.invokeMethod(
+              'setTag', {'key': 'fixture-key', 'value': 'fixture-value'}))
+          .thenAnswer((_) => Future.value());
+
+      final sut = fixture.getSut();
+      await sut.setTag('fixture-key', 'fixture-value');
+
+      verify(fixture.methodChannel.invokeMethod(
+          'setTag', {'key': 'fixture-key', 'value': 'fixture-value'}));
+    });
+
+    test('removeTag', () async {
+      when(fixture.methodChannel
+              .invokeMethod('removeTag', {'key': 'fixture-key'}))
+          .thenAnswer((_) => Future.value());
+
+      final sut = fixture.getSut();
+      await sut.removeTag('fixture-key');
+
+      verify(fixture.methodChannel
+          .invokeMethod('removeTag', {'key': 'fixture-key'}));
+    });
   });
 }
 
