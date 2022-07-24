b978495ae9804b9ec0b3d779cb1cabff87ee38a7
Jonas Uekötter <dev@uekoetter.dev>
2022-07-24 11:09:48 +0200

    default handled is true

--- flutter/lib/src/integrations/on_error_integration.dart
+++ flutter/lib/src/integrations/on_error_integration.dart
@@ -38,7 +38,7 @@ class OnErrorIntegration implements Integration<SentryFlutterOptions> {
     _defaultOnError = wrapper.onError;
 
     _integrationOnError = (Object exception, StackTrace stackTrace) {
-      final handled = _defaultOnError?.call(exception, stackTrace) ?? false;
+      final handled = _defaultOnError?.call(exception, stackTrace) ?? true;
 
       // As per docs, the app might crash on some platforms
       // after this is called.
