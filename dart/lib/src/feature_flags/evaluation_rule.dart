14d7c9cd57b28b994e70129b5a957d2b1e290c93
Manoel Aranda Neto <marandaneto@gmail.com>
2022-08-22 14:26:22 +0200

    add type enum and rename evaluation to evaluation rule

--- dart/lib/src/feature_flags/evaluation_rule.dart
+++ dart/lib/src/feature_flags/evaluation_rule.dart
@@ -1,19 +1,20 @@
 import 'package:meta/meta.dart';
+import 'evaluation_type.dart';
 
 @immutable
-class Evaluation {
-  final String type;
+class EvaluationRule {
+  final EvaluationType type;
   final double? percentage;
   final bool? result;
   final Map<String, dynamic> _tags;
 
   Map<String, dynamic> get tags => Map.unmodifiable(_tags);
 
-  Evaluation(this.type, this.percentage, this.result, this._tags);
+  EvaluationRule(this.type, this.percentage, this.result, this._tags);
 
-  factory Evaluation.fromJson(Map<String, dynamic> json) {
-    return Evaluation(
-      json['type'] as String,
+  factory EvaluationRule.fromJson(Map<String, dynamic> json) {
+    return EvaluationRule(
+      (json['type'] as String).toEvaluationType(),
       json['percentage'] as double?,
       json['result'] as bool?,
       Map<String, dynamic>.from(json['tags'] as Map),
--- dart/lib/src/feature_flags/feature_flag.dart
+++ dart/lib/src/feature_flags/feature_flag.dart
@@ -1,23 +1,23 @@
 import 'package:meta/meta.dart';
 
-import 'evaluation.dart';
+import 'evaluation_rule.dart';
 
 @immutable
 class FeatureFlag {
   final String name;
   final Map<String, dynamic> _tags;
-  final List<Evaluation> _evaluations;
+  final List<EvaluationRule> _evaluations;
 
   Map<String, dynamic> get tags => Map.unmodifiable(_tags);
 
-  List<Evaluation> get evaluations => List.unmodifiable(_evaluations);
+  List<EvaluationRule> get evaluations => List.unmodifiable(_evaluations);
 
   FeatureFlag(this.name, this._tags, this._evaluations);
 
   factory FeatureFlag.fromJson(Map<String, dynamic> json) {
     final evaluationsList = json['evaluation'] as List<dynamic>? ?? [];
     final evaluations = evaluationsList
-        .map((e) => Evaluation.fromJson(e))
+        .map((e) => EvaluationRule.fromJson(e))
         .toList(growable: false);
 
     return FeatureFlag(
