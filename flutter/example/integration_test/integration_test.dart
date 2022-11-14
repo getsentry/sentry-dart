5158fc5e7c8ab545f52171d37428e7688d4940a1
Denis Andrasec <denrase@gmail.com>
2022-11-14 13:20:35 +0100

    remove unused imports

--- flutter/example/integration_test/integration_test.dart
+++ flutter/example/integration_test/integration_test.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-
-import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
