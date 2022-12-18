1cdaee4d6556331369f90dd600aa98744d2fe605
Jonas Uekötter <dev@uekoetter.dev>
2022-12-18 16:51:03 +0100

    return

--- flutter/lib/src/binding_wrapper.dart
+++ flutter/lib/src/binding_wrapper.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/widgets.dart';
+
+import '../sentry_flutter.dart';
 
 // The methods and properties are modelled after the the real binding class.
 class BindingWrapper {
@@ -6,7 +10,20 @@ class BindingWrapper {
   /// Provides access to the features exposed by this mixin.
   /// The binding must be initialized before using this getter;
   /// this is typically done by calling [runApp] or [WidgetsFlutterBinding.ensureInitialized].
-  WidgetsBinding? get instance => _ambiguate(WidgetsBinding.instance);
+  WidgetsBinding? get instance {
+    try {
+      return _ambiguate(WidgetsBinding.instance);
+    } catch (e, s) {
+      log(
+        'WidgetsBinding.instance was not yet initialized',
+        level: SentryLevel.error.toDartLogLevel(),
+        error: e,
+        stackTrace: s,
+        name: 'Sentry',
+      );
+      return null;
+    }
+  }
 
   /// Returns an instance of the binding that implements [WidgetsBinding].
   /// If no binding has yet been initialized, the [WidgetsFlutterBinding] class
