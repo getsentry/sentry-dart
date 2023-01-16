fae536b6ff55e790fc92cc53cb9b7ee33e164a63
Denis Andrasec <denrase@gmail.com>
2023-01-16 10:23:09 +0100

    fix api call

--- flutter/ios/Classes/SentryFlutterPluginApple.swift
+++ flutter/ios/Classes/SentryFlutterPluginApple.swift
@@ -587,7 +587,7 @@ public class SentryFlutterPluginApple: NSObject, FlutterPlugin {
         breadcrumbInstance.data = data
       }
 
-      SentrySDK.addBreadcrumb(breadcrumbInstance)
+      SentrySDK.addBreadcrumb(crumb: breadcrumbInstance)
 
       result("")
     }
