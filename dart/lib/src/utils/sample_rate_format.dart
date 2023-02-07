02b12e09207dbf06bb4037ccd87474509e44ca8e
Denis Andrasec <denrase@gmail.com>
2023-02-07 09:30:29 +0100

    update formatting of > 16 digits

--- dart/lib/src/utils/sample_rate_format.dart
+++ dart/lib/src/utils/sample_rate_format.dart
@@ -1,9 +1,17 @@
+import 'dart:math';
+
 import 'package:meta/meta.dart';
 
 @internal
 class SampleRateFormat {
   static String format(double sampleRate) {
-    final fixed = sampleRate.toStringAsFixed(16);
+    final rounded = dp(sampleRate, 16);
+    final fixed = rounded.toStringAsFixed(16);
     return fixed.replaceAll(RegExp(r"([.]*0+)(?!.*\d)"), "");
+  }
+
+  static double dp(double val, int places){
+    num mod = pow(10.0, places);
+    return ((val * mod).round().toDouble() / mod);
   }
 }
