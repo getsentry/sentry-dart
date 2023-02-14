81c2f69d1be9112d99488a12376b57e1d2b99859
Jonas Uekötter <ueman@users.noreply.github.com>
2023-02-14 12:53:31 +0100

    fix tests

--- dio/test/dio_event_processor_test.dart
+++ dio/test/dio_event_processor_test.dart
@@ -75,7 +75,6 @@ void main() {
       expect(processedEvent.request?.queryString, 'foo=bar');
       expect(processedEvent.request?.headers, <String, String>{
         'foo': 'bar',
-        'content-type': 'application/json; charset=utf-8'
       });
       expect(processedEvent.request?.data, 'foobar');
     });
