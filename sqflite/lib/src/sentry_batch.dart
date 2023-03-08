624657797787450c1854fbb338e7afedf098f596
Manoel Aranda Neto <marandaneto@gmail.com>
2023-03-08 12:24:13 +0100

    fix deps

--- sqflite/lib/src/sentry_batch.dart
+++ sqflite/lib/src/sentry_batch.dart
@@ -2,7 +2,7 @@ import 'package:meta/meta.dart';
 import 'package:sentry/sentry.dart';
 import 'package:sqflite/sqflite.dart';
 
-// ignore: implementation_imports, depend_on_referenced_packages
+// ignore: implementation_imports
 import 'package:sqflite_common/src/sql_builder.dart';
 
 /// A [Batch] wrapper that adds Sentry support.
