a6c782ad485e7df43302e5f1a651e666eabedc6f
Manoel Aranda Neto <marandaneto@gmail.com>
2023-03-20 14:12:54 +0100

    remove comment

--- flutter/example/lib/main.dart
+++ flutter/example/lib/main.dart
@@ -9,7 +9,6 @@ import 'package:flutter/services.dart';
 import 'package:logging/logging.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:sentry_sqflite/sentry_sqflite.dart';
-// import 'package:sentry_sqflite/sentry_sqflite.dart';
 import 'package:sqflite/sqflite.dart';
 // import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 // import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
