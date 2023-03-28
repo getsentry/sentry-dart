f15574e523b05395bbfefff40936e71b35bd7eba
Denis Andrasec <denrase@gmail.com>
2023-03-28 14:36:46 +0200

    add documentation

--- file/lib/src/sentry_io_overrides_integration.dart
+++ file/lib/src/sentry_io_overrides_integration.dart
@@ -4,13 +4,19 @@ import 'dart:io';
 import 'package:sentry/sentry.dart';
 import 'sentry_io_overrides.dart';
 
+/// When installed, every new file will be created as [SentryFile].
+/// When installed, operations will use [SentryFile] instead of dart:io's [File]
+/// implementation whenever [File] is used.
+///
+/// Must be called outside the RunZoneGuardedIntegration.
+/// Otherwise the FileOverrides aren't attached to the root zone.
 class SentryIOOverridesIntegration extends Integration<SentryOptions> {
   IOOverrides? _previousOverrides;
 
   @override
   FutureOr<void> call(Hub hub, SentryOptions options) {
-    _previousOverrides = IOOverrides.current;
     if (options.isTracingEnabled()) {
+      _previousOverrides = IOOverrides.current;
       IOOverrides.global = SentryIOOverrides(hub);
       options.sdk.addIntegration('sentryIOOverridesIntegration');
     }
