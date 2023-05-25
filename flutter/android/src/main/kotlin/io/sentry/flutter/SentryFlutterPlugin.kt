973e3a2e411182c7eb3abb030a7b34be040b2ce7
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2023-05-26 07:47:18 +0800

    Update SentryFlutterPlugin.kt

--- flutter/android/src/main/kotlin/io/sentry/flutter/SentryFlutterPlugin.kt
+++ flutter/android/src/main/kotlin/io/sentry/flutter/SentryFlutterPlugin.kt
@@ -191,7 +191,7 @@ class SentryFlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
       }
 
       args.getIfNotNull<Int>("connectionTimeoutMillis") { options.connectionTimeoutMillis = it }
-      args.getIfNotNull<Int>("readTimeoutMillis") { options.connectionTimeoutMillis = it }
+      args.getIfNotNull<Int>("readTimeoutMillis") { options.readTimeoutMillis = it }
 
       // missing proxy
     }
