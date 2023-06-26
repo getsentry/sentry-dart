bfcaae97a174ab5c6ed31c6bb36541713eb45403
Denis Andrasec <denrase@gmail.com>
2023-06-26 11:21:55 +0200

    add missing origin

--- dart/lib/src/sentry_trace_origins.dart
+++ dart/lib/src/sentry_trace_origins.dart
@@ -8,5 +8,6 @@ class SentryTraceOrigins {
   static const autoHttpHttpTracingClient = 'auto.http.http.tracing_client';
   static const autoHttpDioTracingClientAdapter = 'auto.http.dio.tracing_client_adapter';
   static const autoHttpDioSentryTransformer = 'auto.http.dio.sentry_transformer';
+  static const autoFile = 'auto.file';
   static const autoFileAssetBundle = 'auto.file.asset_bundle';
 }
