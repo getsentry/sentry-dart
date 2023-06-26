f2d527a7ddbd28cab6b25600169a17ba148505e3
Denis Andrasec <denrase@gmail.com>
2023-06-26 10:47:43 +0200

    set origin in span asset bundle

--- dart/lib/src/sentry_trace_origins.dart
+++ dart/lib/src/sentry_trace_origins.dart
@@ -7,5 +7,5 @@ class SentryTraceOrigins {
   static const autoNavigationSentryNavigatorObserver = 'auto.navigation.sentry_navigator_observer';
   static const autoHttpHttpTracingClient = 'auto.http.http.tracing_client';
   static const autoHttpDioTracingClientAdapter = 'auto.http.dio.tracing_client_adapter';
-  static const autoFile = 'auto.file';
+  static const autoFileAssetBundle = 'auto.file.asset_bundle';
 }
