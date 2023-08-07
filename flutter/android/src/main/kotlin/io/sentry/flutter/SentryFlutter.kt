57a189ed445ef8df2eb0047862196243c333997b
Denis Andrasec <denrase@gmail.com>
2023-08-07 14:35:29 +0200

    update remaining options for android

--- flutter/android/src/main/kotlin/io/sentry/flutter/SentryFlutter.kt
+++ flutter/android/src/main/kotlin/io/sentry/flutter/SentryFlutter.kt
@@ -1,15 +1,84 @@
 package io.sentry.flutter
 
-import io.sentry.android.core.SentryAndroid
+import io.sentry.SentryLevel
+import io.sentry.android.core.BuildConfig
 import io.sentry.android.core.SentryAndroidOptions
+import io.sentry.protocol.SdkVersion
+import java.util.Locale
 
 class SentryFlutter(
-  private val options: SentryAndroidOptions
+  private val androidSdk: String,
+  private val nativeSdk: String
 ) {
-  fun initNativeSdk(data: Map<String, Any>) {
+
+  var autoPerformanceTracingEnabled = false
+
+  fun updateOptions(options: SentryAndroidOptions, data: Map<String, Any>) {
     data.getIfNotNull<String>("dsn") {
       options.dsn = it
     }
+    data.getIfNotNull<Boolean>("debug") { options.isDebug = it }
+    data.getIfNotNull<String>("environment") { options.environment = it }
+    data.getIfNotNull<String>("release") { options.release = it }
+    data.getIfNotNull<String>("dist") { options.dist = it }
+    data.getIfNotNull<Boolean>("enableAutoSessionTracking") { options.isEnableAutoSessionTracking = it }
+    data.getIfNotNull<Long>("autoSessionTrackingIntervalMillis") { options.sessionTrackingIntervalMillis = it }
+    data.getIfNotNull<Long>("anrTimeoutIntervalMillis") { options.anrTimeoutIntervalMillis = it }
+    data.getIfNotNull<Boolean>("attachThreads") { options.isAttachThreads = it }
+    data.getIfNotNull<Boolean>("attachStacktrace") { options.isAttachStacktrace = it }
+    data.getIfNotNull<Boolean>("enableAutoNativeBreadcrumbs") {
+      options.isEnableActivityLifecycleBreadcrumbs = it
+      options.isEnableAppLifecycleBreadcrumbs = it
+      options.isEnableSystemEventBreadcrumbs = it
+      options.isEnableAppComponentBreadcrumbs = it
+      options.isEnableUserInteractionBreadcrumbs = it
+    }
+    data.getIfNotNull<Int>("maxBreadcrumbs") { options.maxBreadcrumbs = it }
+    data.getIfNotNull<Int>("maxCacheItems") { options.maxCacheItems = it }
+    data.getIfNotNull<String>("diagnosticLevel") {
+      if (options.isDebug) {
+        val sentryLevel = SentryLevel.valueOf(it.toUpperCase(Locale.ROOT))
+        options.setDiagnosticLevel(sentryLevel)
+      }
+    }
+    data.getIfNotNull<Boolean>("anrEnabled") { options.isAnrEnabled = it }
+    data.getIfNotNull<Boolean>("sendDefaultPii") { options.isSendDefaultPii = it }
+    data.getIfNotNull<Boolean>("enableNdkScopeSync") { options.isEnableScopeSync = it }
+    data.getIfNotNull<String>("proguardUuid") { options.proguardUuid = it }
+
+    val nativeCrashHandling = (data["enableNativeCrashHandling"] as? Boolean) ?: true
+    // nativeCrashHandling has priority over anrEnabled
+    if (!nativeCrashHandling) {
+      options.isEnableUncaughtExceptionHandler = false
+      options.isAnrEnabled = false
+      // if split symbols are enabled, we need Ndk integration so we can't really offer the option
+      // to turn it off
+      // options.isEnableNdk = false
+    }
+
+    data.getIfNotNull<Boolean>("enableAutoPerformanceTracing") { enableAutoPerformanceTracing ->
+      if (enableAutoPerformanceTracing) {
+        autoPerformanceTracingEnabled = true
+      }
+    }
+
+    data.getIfNotNull<Boolean>("sendClientReports") { options.isSendClientReports = it }
+
+    data.getIfNotNull<Long>("maxAttachmentSize") { options.maxAttachmentSize = it }
+
+    var sdkVersion = options.sdkVersion
+    if (sdkVersion == null) {
+      sdkVersion = SdkVersion(androidSdk, BuildConfig.VERSION_NAME)
+    } else {
+      sdkVersion.name = androidSdk
+    }
+
+    options.sdkVersion = sdkVersion
+    options.sentryClientName = "$androidSdk/${BuildConfig.VERSION_NAME}"
+    options.nativeSdkName = nativeSdk
+
+    data.getIfNotNull<Int>("connectionTimeoutMillis") { options.connectionTimeoutMillis = it }
+    data.getIfNotNull<Int>("readTimeoutMillis") { options.readTimeoutMillis = it }
   }
 }
 
--- flutter/android/src/test/kotlin/io/sentry/flutter/SentryFlutterTest.kt
+++ flutter/android/src/test/kotlin/io/sentry/flutter/SentryFlutterTest.kt
@@ -1,5 +1,7 @@
 package io.sentry.flutter
 
+import io.sentry.SentryLevel
+import io.sentry.android.core.BuildConfig
 import io.sentry.android.core.SentryAndroidOptions
 import org.junit.Assert.assertEquals
 import org.junit.Before
@@ -16,11 +18,100 @@ class SentryFlutterTest {
 
   @Test
   fun initNativeSkd() {
-    val sut = fixture.getSut();
-    sut.initNativeSdk(mapOf(
-      "dsn" to "fixture-dsn"
+    // Given
+    val sut = fixture.getSut()
+
+    // When
+    sut.updateOptions(fixture.options, mapOf(
+      "dsn" to "fixture-dsn",
+      "debug" to true,
+      "environment" to "fixture-environment",
+      "release" to "fixture-release",
+      "dist" to "fixture-dist",
+      "enableAutoSessionTracking" to false,
+      "autoSessionTrackingIntervalMillis" to 9001L,
+      "anrTimeoutIntervalMillis" to 9002L,
+      "attachThreads" to true,
+      "attachStacktrace" to false,
+      "enableAutoNativeBreadcrumbs" to false,
+      "maxBreadcrumbs" to 9003,
+      "maxCacheItems" to 9004,
+      "anrEnabled" to false,
+      "sendDefaultPii" to true,
+      "enableNdkScopeSync" to false,
+      "proguardUuid" to "fixture-proguardUuid",
+      "enableNativeCrashHandling" to false,
+      "sendClientReports" to false,
+      "maxAttachmentSize" to 9005L,
+      "enableAutoPerformanceTracing" to true,
+      "connectionTimeoutMillis" to 9006,
+      "readTimeoutMillis" to 9007,
     ))
+
+    // Then
     assertEquals("fixture-dsn", fixture.options.dsn)
+    assertEquals(true, fixture.options.isDebug)
+    assertEquals("fixture-environment", fixture.options.environment)
+    assertEquals("fixture-release", fixture.options.release)
+    assertEquals("fixture-dist", fixture.options.dist)
+    assertEquals(false, fixture.options.isEnableAutoSessionTracking)
+    assertEquals(9001L, fixture.options.sessionTrackingIntervalMillis)
+    assertEquals(9002L, fixture.options.anrTimeoutIntervalMillis)
+    assertEquals(true, fixture.options.isAttachThreads)
+    assertEquals(false, fixture.options.isAttachStacktrace)
+    assertEquals(false, fixture.options.isEnableActivityLifecycleBreadcrumbs)
+    assertEquals(false, fixture.options.isEnableAppLifecycleBreadcrumbs)
+    assertEquals(false, fixture.options.isEnableSystemEventBreadcrumbs)
+    assertEquals(false, fixture.options.isEnableAppComponentBreadcrumbs)
+    assertEquals(false, fixture.options.isEnableUserInteractionBreadcrumbs)
+    assertEquals(9003, fixture.options.maxBreadcrumbs)
+    assertEquals(9004, fixture.options.maxCacheItems)
+    assertEquals(false, fixture.options.isAnrEnabled)
+    assertEquals(true, fixture.options.isSendDefaultPii)
+    assertEquals(false, fixture.options.isEnableScopeSync)
+    assertEquals("fixture-proguardUuid", fixture.options.proguardUuid)
+    assertEquals(false, fixture.options.isSendClientReports)
+    assertEquals(9005L, fixture.options.maxAttachmentSize)
+
+    assertEquals("sentry.java.android.flutter", fixture.options.sdkVersion?.name)
+    assertEquals(BuildConfig.VERSION_NAME, fixture.options.sdkVersion?.version)
+    assertEquals("sentry.java.android.flutter/${BuildConfig.VERSION_NAME}", fixture.options.sentryClientName)
+    assertEquals("fixture-nativeSdk", fixture.options.nativeSdkName)
+
+    assertEquals(true, sut.autoPerformanceTracingEnabled)
+
+    assertEquals(9006, fixture.options.connectionTimeoutMillis)
+    assertEquals(9007, fixture.options.readTimeoutMillis)
+  }
+
+  @Test
+  fun initNativeSdkDiagnosticLevel() {
+    // Given
+    val sut = fixture.getSut()
+    fixture.options.isDebug = true
+
+    // When
+    sut.updateOptions(fixture.options, mapOf(
+      "diagnosticLevel" to "warning",
+    ))
+
+    // Then
+    assertEquals(SentryLevel.WARNING, fixture.options.diagnosticLevel)
+  }
+
+  @Test
+  fun initNativeSdkEnableNativeCrashHandling() {
+    // Given
+    val sut = fixture.getSut()
+
+    // When
+    sut.updateOptions(fixture.options, mapOf(
+      "enableNativeCrashHandling" to false,
+    ))
+
+    // Then
+    assertEquals(false, fixture.options.isEnableUncaughtExceptionHandler)
+    assertEquals(false, fixture.options.isAnrEnabled)
   }
 }
 
@@ -29,7 +120,9 @@ class Fixture {
   var options = SentryAndroidOptions()
 
   fun getSut(): SentryFlutter {
-    return SentryFlutter(options = options)
+    return SentryFlutter(
+      androidSdk = "sentry.java.android.flutter",
+      nativeSdk = "fixture-nativeSdk",
+    )
   }
-
 }
