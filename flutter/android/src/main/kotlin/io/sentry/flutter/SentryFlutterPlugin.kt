e884aba7e03ec56ab2dd7277b50f7dac2bec2680
Denis Andrasec <denrase@gmail.com>
2023-09-04 13:09:56 +0200

    remove unused import

--- flutter/android/src/main/kotlin/io/sentry/flutter/SentryFlutterPlugin.kt
+++ flutter/android/src/main/kotlin/io/sentry/flutter/SentryFlutterPlugin.kt
@@ -10,7 +10,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.StandardMessageCodec
 import io.sentry.Breadcrumb
 import io.sentry.DateUtils
 import io.sentry.Hint
