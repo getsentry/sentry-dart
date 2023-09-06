b49636692caaaf65c82c640d8701357991e29baf
buenaflor <giancarlo.buenaflor@sentry.io>
2023-09-06 10:59:06 +0200

    fix score

--- dio/lib/src/tracing_client_adapter.dart
+++ dio/lib/src/tracing_client_adapter.dart
@@ -57,13 +57,15 @@ class TracingClientAdapter implements HttpClientAdapter {
           _hub.options.tracePropagationTargets,
           options.uri.toString(),
         )) {
-          addSentryTraceHeaderFromSpan(span, options.headers);
-          addBaggageHeaderFromSpan(
-            span,
-            options.headers,
-            // ignore: invalid_use_of_internal_member
-            logger: _hub.options.logger,
-          );
+          addSentryTraceHeader(span.toSentryTrace(), options.headers);
+          if (span.toBaggageHeader() != null) {
+            addBaggageHeader(
+              span.toBaggageHeader()!,
+              options.headers,
+              // ignore: invalid_use_of_internal_member
+              logger: _hub.options.logger,
+            );
+          }
         }
       }
 
