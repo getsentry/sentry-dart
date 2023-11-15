266282449130e0a662b64a62ebd7a2e973d4dfec
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2023-11-15 15:33:46 +0100

    Add version to drift and hive

--- drift/lib/src/version.dart
+++ drift/lib/src/version.dart
@@ -2,4 +2,4 @@
 const String sdkVersion = '7.13.0';
 
 /// The package name reported to Sentry.io in the submitted events.
-const String packageName = 'pub:sentry_hive';
+const String packageName = 'pub:sentry_drift';
