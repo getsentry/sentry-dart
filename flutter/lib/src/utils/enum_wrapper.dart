fb4789f27fcd2d4180ebc94bc2b732337b4663d6
Robby Pratama <javavertigogo@gmail.com>
2023-11-21 15:49:35 +0700

    provide missing deprececated function: describeEnum

---
+++ flutter/lib/src/utils/enum_wrapper.dart
@@ -0,0 +1,12 @@
+String describeEnum(Object enumEntry) {
+  if (enumEntry is Enum) {
+    return enumEntry.name;
+  }
+  final String description = enumEntry.toString();
+  final int indexOfDot = description.indexOf('.');
+  assert(
+    indexOfDot != -1 && indexOfDot < description.length - 1,
+    'The provided object "$enumEntry" is not an enum.',
+  );
+  return description.substring(indexOfDot + 1);
+}
