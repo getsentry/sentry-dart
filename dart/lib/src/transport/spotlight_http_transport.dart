3a985bd3e5569c06e8f4f0b2065e25da57465f73
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2023-12-14 16:57:26 +0100

    update spotlight transport

--- dart/lib/src/transport/spotlight_http_transport.dart
+++ dart/lib/src/transport/spotlight_http_transport.dart
@@ -1,11 +1,14 @@
 import 'package:http/http.dart';
 
 import '../../sentry.dart';
+import '../client_reports/discard_reason.dart';
+import 'data_category.dart';
 
 /// Spotlight HTTP transport class that sends Sentry envelopes to both Sentry and Spotlight.
 class SpotlightHttpTransport extends Transport {
   final SentryOptions _options;
   final Transport _transport;
+  final Map<String, String> _headers = {'Content-Type': 'application/x-sentry-envelope'};
 
   SpotlightHttpTransport(this._options, this._transport);
 
@@ -20,10 +23,38 @@ class SpotlightHttpTransport extends Transport {
     final StreamedRequest spotlightRequest =
         StreamedRequest('POST', spotlightUri);
 
-    try {
-      await _options.httpClient.send(spotlightRequest);
-    } catch (e) {
-      // Handle any exceptions.
+    envelope
+        .envelopeStream(_options)
+        .listen(spotlightRequest.sink.add)
+        .onDone(spotlightRequest.sink.close);
+
+    spotlightRequest.headers.addAll(_headers);
+
+    final response = await _options.httpClient
+        .send(spotlightRequest)
+        .then(Response.fromStream);
+
+    if (response.statusCode != 200) {
+      // body guard to not log the error as it has performance impact to allocate
+      // the body String.
+      if (_options.debug) {
+        _options.logger(
+          SentryLevel.error,
+          'Spotlight Sidecar API returned an error, statusCode = ${response.statusCode}, '
+          'body = ${response.body}',
+        );
+        print('body = ${response.request}');
+      }
+
+      if (response.statusCode >= 400 && response.statusCode != 429) {
+        _options.recorder.recordLostEvent(
+            DiscardReason.networkError, DataCategory.error);
+      }
+    } else {
+      _options.logger(
+        SentryLevel.debug,
+        'Envelope ${envelope.header.eventId ?? "--"} was sent successfully to spotlight ($spotlightUri)',
+      );
     }
   }
 }
