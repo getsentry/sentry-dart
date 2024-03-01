34033f9af54e6bc646bbb5e233a11e752e1c0351
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2024-03-01 10:49:35 +0100

    remove debug label from framecallbackhandler

--- flutter/lib/src/frame_callback_handler.dart
+++ flutter/lib/src/frame_callback_handler.dart
@@ -2,13 +2,12 @@ import 'package:flutter/scheduler.dart';
 import 'package:flutter/widgets.dart';
 
 abstract class FrameCallbackHandler {
-  void addPostFrameCallback(FrameCallback callback, {String debugLabel});
+  void addPostFrameCallback(FrameCallback callback);
 }
 
 class DefaultFrameCallbackHandler implements FrameCallbackHandler {
   @override
-  void addPostFrameCallback(FrameCallback callback,
-      {String debugLabel = 'callback'}) {
+  void addPostFrameCallback(FrameCallback callback) {
     WidgetsBinding.instance.addPostFrameCallback(callback);
   }
 }
