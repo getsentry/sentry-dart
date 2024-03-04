6b0edd64aacdb068135b29c683e4cc0c6b1841fc
Denis Andrasec <denrase@gmail.com>
2024-03-04 13:40:47 +0100

    Add documentation

--- dart/lib/src/sentry_options.dart
+++ dart/lib/src/sentry_options.dart
@@ -83,8 +83,13 @@ class SentryOptions {
 
   int _maxQueueSize = 30;
 
+  /// Returns the max number of events Sentry will send when calling capture
+  /// methods in a tight loop. Default is 30.
   int get maxQueueSize => _maxQueueSize;
 
+  /// Sets how many unawaited events can be sent by Sentry. (e.g. capturing
+  /// events in a tight loop) at once. If you need to send more, please use the
+  /// await keyword.
   set maxQueueSize(int count) {
     assert(count > 0);
     _maxQueueSize = count;
