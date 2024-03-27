271e33bb53a01b40f0d1d43c37f183383419dee9
stefanosiano <stefano.siano91@gmail.com>
2024-03-27 13:32:18 +0100

    added SentryOptions.beforeMetricCallback added beforeMetricCallback logic in metrics_aggregator.dart added timing metric api with span auto start

--- dart/test/metrics/metrics_api_test.dart
+++ dart/test/metrics/metrics_api_test.dart
@@ -1,8 +1,10 @@
+import 'package:sentry/sentry.dart';
 import 'package:sentry/src/metrics/metric.dart';
 import 'package:sentry/src/metrics/metrics_api.dart';
-import 'package:test/expect.dart';
-import 'package:test/scaffolding.dart';
+import 'package:sentry/src/sentry_tracer.dart';
+import 'package:test/test.dart';
 
+import '../mocks.dart';
 import '../mocks/mock_hub.dart';
 
 void main() {
@@ -68,11 +70,66 @@ void main() {
       expect(sentMetrics.first.type, MetricType.set);
       expect((sentMetrics.first as SetMetric).values, {1, 2, 4, 494360628});
     });
+
+    test('timing emits distribution', () async {
+      final delay = Duration(milliseconds: 100);
+      MetricsApi api = fixture.getSut();
+      int count = 0;
+
+      // The timing API tries to start a child span
+      expect(fixture.mockHub.getSpanCalls, 0);
+      api.timing('key', function: () => Future.delayed(delay, () => count++));
+      expect(fixture.mockHub.getSpanCalls, 1);
+
+      await Future.delayed(delay);
+      Iterable<Metric> sentMetrics =
+          fixture.mockHub.metricsAggregator!.buckets.values.first.values;
+
+      // The timing API emits a distribution metric
+      expect(sentMetrics.first.type, MetricType.distribution);
+      // The default unit is second
+      expect(sentMetrics.first.unit, DurationSentryMeasurementUnit.second);
+      // It awaits for the function completion, which means 100 milliseconds in
+      // this case. Since the unit is second, its value (duration) is > 0.1
+      expect(
+          (sentMetrics.first as DistributionMetric).values.first > 0.1, true);
+    });
+
+    test('timing starts a span', () async {
+      final delay = Duration(milliseconds: 100);
+      fixture._options.tracesSampleRate = 1;
+      MetricsApi api = fixture.getSut(hub: fixture.hub);
+
+      // Start a transaction so that timing api can start a child span
+      final t = fixture.hub.startTransaction(
+        'name',
+        'operation',
+        bindToScope: true,
+      ) as SentryTracer;
+      expect(t.children, isEmpty);
+
+      // Timing starts a span
+      api.timing('my key', function: () => Future.delayed(delay, () => {}));
+      final span = t.children.first;
+      expect(span.finished, false);
+      expect(span.context.operation, 'metric.timing');
+      expect(span.context.description, 'my key');
+
+      // Timing finishes the span when the function is finished, which takes 100 milliseconds
+      await Future.delayed(delay);
+      expect(
+        span.endTimestamp!.difference(span.startTimestamp).inMilliseconds > 100,
+        true,
+      );
+      expect(span.finished, true);
+    });
   });
 }
 
 class Fixture {
+  final _options = SentryOptions(dsn: fakeDsn);
   final mockHub = MockHub();
+  late final hub = Hub(_options);
 
-  MetricsApi getSut() => MetricsApi(hub: mockHub);
+  MetricsApi getSut({Hub? hub}) => MetricsApi(hub: hub ?? mockHub);
 }
