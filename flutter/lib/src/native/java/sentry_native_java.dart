3f6d05eb802f0ff9f392788711ba4504f3c9c746
Ivan Dlugos <dlugos.ivan@gmail.com>
2024-05-06 13:32:13 +0200

    remove jni/jnigen

--- flutter/lib/src/native/java/sentry_native_java.dart
+++ flutter/lib/src/native/java/sentry_native_java.dart
@@ -1,24 +1,10 @@
-import 'package:jni/jni.dart';
 import 'package:meta/meta.dart';
 
-import '../../sentry_flutter_options.dart';
 import '../sentry_native_channel.dart';
-import 'android_replay_recorder.dart';
-import 'binding.dart' as java;
 
+// Note: currently this doesn't do anything. Later, it shall be used with
+// generated JNI bindings. See https://github.com/getsentry/sentry-dart/issues/1444
 @internal
 class SentryNativeJava extends SentryNativeChannel {
   SentryNativeJava(super.channel);
-
-  @override
-  Future<void> init(SentryFlutterOptions options) async {
-    if (options.replay.isEnabled) {
-      // Necessary for the generated binding to work as of jnigen v0.6.0
-      // This may change in the future.
-      Jni.initDLApi();
-
-      java.SentryFlutterReplay.recorder = AndroidReplayRecorder.create(options);
-    }
-    return super.init(options);
-  }
 }
