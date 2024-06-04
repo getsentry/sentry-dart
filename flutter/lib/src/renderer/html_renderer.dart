9a156fbf4b45c020b3ad0fcaf4e1c985a4b4055d
Josh Burton <josh@arklabs.nz>
2024-06-05 10:21:34 +1200

    fix: enhances comment

--- flutter/lib/src/renderer/html_renderer.dart
+++ flutter/lib/src/renderer/html_renderer.dart
@@ -10,6 +10,9 @@ bool get isCanvasKitRenderer {
   return _windowFlutterCanvasKit != null;
 }
 
+// These values are set by the engine. They are used to determine if the
+// application is using canvaskit or skwasm.
+//
 // See https://github.com/flutter/flutter/blob/414d9238720a3cde85475f49ce0ba313f95046f7/packages/flutter/lib/src/foundation/_capabilities_web.dart#L10
 @JS('window.flutterCanvasKit')
 external JSAny? get _windowFlutterCanvasKit;
