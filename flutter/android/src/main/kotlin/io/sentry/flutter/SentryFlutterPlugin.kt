47021acbd1d4e6a6d06c72dc63b9d0bfaf8718ef
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2024-07-08 12:53:04 +0200

    Revert SentryFlutterPlugin.kt to previous commit

--- flutter/android/src/main/kotlin/io/sentry/flutter/SentryFlutterPlugin.kt
+++ flutter/android/src/main/kotlin/io/sentry/flutter/SentryFlutterPlugin.kt
@@ -356,9 +356,7 @@ class SentryFlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
     if (args.isNotEmpty()) {
       val event = args.first() as ByteArray?
       if (event != null && event.isNotEmpty()) {
-        // Flutter event do not crash the application, we always want a new session to start
-        // after event with exception.handled = false
-        val id = InternalSentrySdk.captureEnvelope(event, true)
+        val id = InternalSentrySdk.captureEnvelope(event)
         if (id != null) {
           result.success("")
         } else {
