7f87af99fa5e407afdd004ed4d42073a5d293413
Denis Andrasec <denrase@gmail.com>
2024-07-09 14:39:57 +0200

    Add `unknown` map to `SdkInfo `

--- dart/lib/src/protocol/sdk_info.dart
+++ dart/lib/src/protocol/sdk_info.dart
@@ -1,5 +1,7 @@
 import 'package:meta/meta.dart';
 
+import 'unknown.dart';
+
 /// An object describing the system SDK.
 @immutable
 class SdkInfo {
@@ -8,11 +10,15 @@ class SdkInfo {
   final int? versionMinor;
   final int? versionPatchlevel;
 
+  @internal
+  final Map<String, dynamic>? unknown;
+
   const SdkInfo({
     this.sdkName,
     this.versionMajor,
     this.versionMinor,
     this.versionPatchlevel,
+    this.unknown,
   });
 
   /// Deserializes a [SdkInfo] from JSON [Map].
@@ -22,17 +28,25 @@ class SdkInfo {
       versionMajor: json['version_major'],
       versionMinor: json['version_minor'],
       versionPatchlevel: json['version_patchlevel'],
+      unknown: unknownFrom(json, {
+        'sdk_name',
+        'version_major',
+        'version_minor',
+        'version_patchlevel',
+      }),
     );
   }
 
   /// Produces a [Map] that can be serialized to JSON.
   Map<String, dynamic> toJson() {
-    return {
+    final json = {
       if (sdkName != null) 'sdk_name': sdkName,
       if (versionMajor != null) 'version_major': versionMajor,
       if (versionMinor != null) 'version_minor': versionMinor,
       if (versionPatchlevel != null) 'version_patchlevel': versionPatchlevel,
     };
+    json.addAll(unknown ?? {});
+    return json;
   }
 
   SdkInfo copyWith({
@@ -46,5 +60,6 @@ class SdkInfo {
         versionMajor: versionMajor ?? this.versionMajor,
         versionMinor: versionMinor ?? this.versionMinor,
         versionPatchlevel: versionPatchlevel ?? this.versionPatchlevel,
+        unknown: unknown,
       );
 }
