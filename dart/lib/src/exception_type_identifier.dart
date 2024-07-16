34c4e114b04bcdf90a40985b24eca6a3ed02ba72
Giancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2024-07-16 16:46:43 +0200

    Update exception_type_identifier.dart

--- dart/lib/src/exception_type_identifier.dart
+++ dart/lib/src/exception_type_identifier.dart
@@ -1,9 +1,7 @@
 /// An abstract class for identifying the type of Dart errors and exceptions.
 ///
-/// This class provides a contract for implementing error type identification
-/// in Dart. It's used in scenarios where error types need to be
-/// determined in obfuscated builds, as [runtimeType] is not reliable in
-/// such cases.
+/// It's used in scenarios where error types need to be determined in obfuscated builds
+/// as [runtimeType] is not reliable in such cases.
 ///
 /// Implement this class to create custom error type identifiers for errors or exceptions.
 /// that we do not support out of the box.
@@ -12,10 +10,10 @@
 ///
 /// Example:
 /// ```dart
-/// class MyErrorTypeIdentifier implements ErrorTypeIdentifier {
+/// class MyExceptionTypeIdentifier implements ExceptionTypeIdentifier {
 ///   @override
-///   String? identifyType(dynamic error) {
-///     if (error is MyCustomError) return 'MyCustomError';
+///   String? identifyType(dynamic throwable) {
+///     if (throwable is MyCustomError) return 'MyCustomError';
 ///     return null;
 ///   }
 /// }
