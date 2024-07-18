51e25c03251e90b34e9e7d2753cab26fe96864a2
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2024-07-18 17:07:25 +0200

    update

--- dart/lib/src/dart_exception_type_identifier_web.dart
+++ dart/lib/src/dart_exception_type_identifier_web.dart
@@ -1,8 +1,6 @@
-import 'dart:html';
 import 'package:meta/meta.dart';
 
 @internal
 String? identifyPlatformSpecificException(dynamic throwable) {
-  if (throwable is NullWindowException) return 'NullWindowException';
   return null;
 }
