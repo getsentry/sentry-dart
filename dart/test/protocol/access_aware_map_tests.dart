91458ee7270f5643eae926d3d27641f223f30180
Denis Andrasec <denrase@gmail.com>
2024-07-29 11:35:04 +0200

    add tests for mapbase

--- dart/test/protocol/access_aware_map_tests.dart
+++ dart/test/protocol/access_aware_map_tests.dart
@@ -1,7 +1,51 @@
+import 'package:collection/collection.dart';
 import 'package:sentry/src/protocol/access_aware_map.dart';
 import 'package:test/test.dart';
 
 void main() {
+  group('MapBase', () {
+    test('set/get value for key', () {
+      final sut = AccessAwareMap({
+        'foo': 'foo',
+      });
+
+      sut['foo'] = 'bar';
+      sut['bar'] = 'foo';
+
+      expect(sut['foo'], 'bar');
+      expect(sut['bar'], 'foo');
+    });
+
+    test('clear', () {
+      final sut = AccessAwareMap({
+        'foo': 'foo',
+      });
+
+      sut.clear();
+
+      expect(sut.isEmpty, true);
+    });
+
+    test('keys', () {
+      final sut = AccessAwareMap({
+        'foo': 'foo',
+        'bar': 'bar',
+      });
+      expect(
+          sut.keys.sortedBy((it) => it), ['bar', 'foo'].sortedBy((it) => it));
+    });
+
+    test('remove', () {
+      final sut = AccessAwareMap({
+        'foo': 'foo',
+      });
+
+      sut.remove('foo');
+
+      expect(sut.isEmpty, true);
+    });
+  });
+
   group('access aware', () {
     test('collects accessedKeys', () {
       final sut = AccessAwareMap({
