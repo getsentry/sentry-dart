8523a6d00d1cf3c74bda1c38864e90628cc4d3ee
Martin Haintz <martin.haintz@gmail.com>
2024-08-13 13:59:32 +0200

    add event processor for html

--- flutter/lib/src/event_processor/url_filter/url_filter_event_processor.dart
+++ flutter/lib/src/event_processor/url_filter/url_filter_event_processor.dart
@@ -1,6 +1,7 @@
 import '../../../sentry_flutter.dart';
 import 'io_url_filter_event_processor.dart'
-    if (dart.library.html) 'web_url_filter_event_processor.dart';
+    if (dart.library.html) 'html_url_filter_event_processor.dart'
+    if (dart.library.js_interop) 'web_url_filter_event_processor.dart';
 
 abstract class UrlFilterEventProcessor implements EventProcessor {
   factory UrlFilterEventProcessor(SentryFlutterOptions options) =>
