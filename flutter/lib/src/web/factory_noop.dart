34816592273c65a0973cbaa227344d79ffd0b92d
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2024-08-20 13:54:58 +0200

    Update

--- flutter/lib/src/web/factory_noop.dart
+++ flutter/lib/src/web/factory_noop.dart
@@ -1,6 +1,19 @@
 import '../../sentry_flutter.dart';
 import '../web/sentry_web_binding.dart';
 
-SentryWebBinding createBinding(SentryFlutterOptions options) {
-  throw UnsupportedError("Web binding is not supported on this platform.");
+class NoOpWebInterop implements SentryWebBinding {
+  @override
+  Future<void> captureEnvelope(SentryEnvelope envelope) async {}
+
+  @override
+  Future<void> captureEvent(SentryEvent event) async {}
+
+  @override
+  Future<void> close() async {}
+
+  @override
+  Future<void> init(SentryFlutterOptions options) async {}
 }
+
+SentryWebBinding createBinding(SentryFlutterOptions options) =>
+    NoOpWebInterop();
