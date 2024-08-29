72c28444e0c2b8ba56242090c1a2887c2af7b69b
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2024-08-29 16:58:29 +0200

    fix test

--- dart/test/load_dart_debug_images_integration_test.dart
+++ dart/test/load_dart_debug_images_integration_test.dart
@@ -5,76 +5,89 @@ import 'package:sentry/sentry.dart';
 import 'package:sentry/src/load_dart_debug_images_integration.dart';
 import 'package:test/test.dart';
 
+import 'mocks/mock_platform.dart';
+import 'mocks/mock_platform_checker.dart';
+
 void main() {
   group(LoadDartDebugImagesIntegration(), () {
     late Fixture fixture;
 
-    setUp(() {
-      fixture = Fixture();
-    });
+    final platforms = [
+      MockPlatform.iOS(),
+      MockPlatform.macOS(),
+      MockPlatform.android(),
+    ];
 
-    test('adds itself to sdk.integrations', () {
-      expect(
-        fixture.options.sdk.integrations.contains('loadDartImageIntegration'),
-        true,
-      );
-    });
+    for (final platform in platforms) {
+      setUp(() {
+        fixture = Fixture();
+        fixture.options.platformChecker =
+            MockPlatformChecker(platform: platform);
+      });
 
-    test('Event processor is added to options', () {
-      expect(fixture.options.eventProcessors.length, 1);
-      expect(
-        fixture.options.eventProcessors.first.runtimeType.toString(),
-        '_LoadImageIntegrationEventProcessor',
-      );
-    });
+      test('adds itself to sdk.integrations', () {
+        expect(
+          fixture.options.sdk.integrations.contains('loadDartImageIntegration'),
+          true,
+        );
+      });
 
-    test(
-        'Event processor does not add debug image if symbolication is not needed',
-        () async {
-      final event = _getEvent(needsSymbolication: false);
-      final processor = fixture.options.eventProcessors.first;
-      final resultEvent = await processor.apply(event, Hint());
+      test('Event processor is added to options', () {
+        expect(fixture.options.eventProcessors.length, 1);
+        expect(
+          fixture.options.eventProcessors.first.runtimeType.toString(),
+          '_LoadImageIntegrationEventProcessor',
+        );
+      });
 
-      expect(resultEvent, equals(event));
-    });
+      test(
+          'Event processor does not add debug image if symbolication is not needed',
+          () async {
+        final event = _getEvent(needsSymbolication: false);
+        final processor = fixture.options.eventProcessors.first;
+        final resultEvent = await processor.apply(event, Hint());
 
-    test('Event processor does not add debug image if stackTrace is null',
-        () async {
-      final event = _getEvent();
-      final processor = fixture.options.eventProcessors.first;
-      final resultEvent = await processor.apply(event, Hint());
+        expect(resultEvent, equals(event));
+      });
 
-      expect(resultEvent, equals(event));
-    });
+      test('Event processor does not add debug image if stackTrace is null',
+          () async {
+        final event = _getEvent();
+        final processor = fixture.options.eventProcessors.first;
+        final resultEvent = await processor.apply(event, Hint());
 
-    test(
-        'Event processor does not add debug image if enableDartSymbolication is false',
-        () async {
-      fixture.options.enableDartSymbolication = false;
-      final event = _getEvent();
-      final processor = fixture.options.eventProcessors.first;
-      final resultEvent = await processor.apply(event, Hint());
+        expect(resultEvent, equals(event));
+      });
 
-      expect(resultEvent, equals(event));
-    });
+      test(
+          'Event processor does not add debug image if enableDartSymbolication is false',
+          () async {
+        fixture.options.enableDartSymbolication = false;
+        final event = _getEvent();
+        final processor = fixture.options.eventProcessors.first;
+        final resultEvent = await processor.apply(event, Hint());
 
-    test('Event processor adds debug image when symbolication is needed',
-        () async {
-      final stackTrace = '''
+        expect(resultEvent, equals(event));
+      });
+
+      test('Event processor adds debug image when symbolication is needed',
+          () async {
+        final stackTrace = '''
 *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***
 build_id: 'b680cb890f9e3c12a24b172d050dec73'
 isolate_dso_base: 10000000
 ''';
-      SentryEvent event = _getEvent();
-      final processor = fixture.options.eventProcessors.first;
-      final resultEvent = await processor.apply(
-          event, Hint()..set(hintRawStackTraceKey, stackTrace));
+        SentryEvent event = _getEvent();
+        final processor = fixture.options.eventProcessors.first;
+        final resultEvent = await processor.apply(
+            event, Hint()..set(hintRawStackTraceKey, stackTrace));
 
-      expect(resultEvent?.debugMeta?.images.length, 1);
-      final debugImage = resultEvent?.debugMeta?.images.first;
-      expect(debugImage?.debugId, isNotEmpty);
-      expect(debugImage?.imageAddr, equals('0x10000000'));
-    });
+        expect(resultEvent?.debugMeta?.images.length, 1);
+        final debugImage = resultEvent?.debugMeta?.images.first;
+        expect(debugImage?.debugId, isNotEmpty);
+        expect(debugImage?.imageAddr, equals('0x10000000'));
+      });
+    }
   });
 }
 
