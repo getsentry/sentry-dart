bdedccfd8dece47ffd40755fdaca9216e66ddac3
Ivan Dlugos <dlugos.ivan@gmail.com>
2024-09-07 20:18:05 +0200

    example: allways show console in the windows sample app

--- flutter/example/windows/runner/main.cpp
+++ flutter/example/windows/runner/main.cpp
@@ -9,9 +9,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
-  if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
+  // if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
     CreateAndAttachConsole();
-  }
+  // }
 
   // Initialize COM, so that it is available for use in the library and/or
   // plugins.
