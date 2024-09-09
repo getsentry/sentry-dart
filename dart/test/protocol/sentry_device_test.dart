aaa812b5fa8e68d8c90928f86eaae5d9cf2e4201
Martin Haintz <martin.haintz@gmail.com>
2024-09-09 14:11:30 +0200

    fix sentryDevice dart tests

--- dart/test/protocol/sentry_device_test.dart
+++ dart/test/protocol/sentry_device_test.dart
@@ -5,101 +5,12 @@ import 'package:test/test.dart';
 import '../mocks.dart';
 
 void main() {
-  final testBootTime = DateTime.fromMicrosecondsSinceEpoch(0);
-
-  final sentryDevice = SentryDevice(
-    name: 'testDevice',
-    family: 'testFamily',
-    model: 'testModel',
-    modelId: 'testModelId',
-    arch: 'testArch',
-    batteryLevel: 23.0,
-    manufacturer: 'testOEM',
-    brand: 'testBrand',
-    views: [
-      SentryView(
-        0,
-        orientation: SentryOrientation.landscape,
-        screenDensity: 99.1,
-        screenDpi: 100,
-        screenHeightPixels: 100,
-        screenWidthPixels: 100,
-      )
-    ],
-    online: false,
-    charging: true,
-    lowMemory: false,
-    simulator: true,
-    memorySize: 1234567,
-    freeMemory: 12345,
-    usableMemory: 9876,
-    storageSize: 1234567,
-    freeStorage: 1234567,
-    externalStorageSize: 98765,
-    externalFreeStorage: 98765,
-    bootTime: testBootTime,
-    batteryStatus: 'Unknown',
-    cpuDescription: 'M1 Pro Max Ultra',
-    deviceType: 'Flutter Device',
-    deviceUniqueIdentifier: 'uuid',
-    processorCount: 4,
-    processorFrequency: 1.2,
-    supportsAccelerometer: true,
-    supportsGyroscope: true,
-    supportsAudio: true,
-    supportsLocationService: true,
-    supportsVibration: true,
-    unknown: testUnknown,
-  );
-
-  final sentryDeviceJson = <String, dynamic>{
-    'name': 'testDevice',
-    'family': 'testFamily',
-    'model': 'testModel',
-    'model_id': 'testModelId',
-    'arch': 'testArch',
-    'battery_level': 23.0,
-    'manufacturer': 'testOEM',
-    'brand': 'testBrand',
-    'views': [
-      {
-        'view_id': 0,
-        'orientation': 'landscape',
-        'screen_density': 99.1,
-        'screen_dpi': 100,
-        'screen_height_pixels': 100,
-        'screen_width_pixels': 100,
-      }
-    ],
-    'online': false,
-    'charging': true,
-    'low_memory': false,
-    'simulator': true,
-    'memory_size': 1234567,
-    'free_memory': 12345,
-    'usable_memory': 9876,
-    'storage_size': 1234567,
-    'free_storage': 1234567,
-    'external_storage_size': 98765,
-    'external_free_storage': 98765,
-    'boot_time': testBootTime.toIso8601String(),
-    'battery_status': 'Unknown',
-    'cpu_description': 'M1 Pro Max Ultra',
-    'device_type': 'Flutter Device',
-    'device_unique_identifier': 'uuid',
-    'processor_count': 4,
-    'processor_frequency': 1.2,
-    'supports_accelerometer': true,
-    'supports_gyroscope': true,
-    'supports_audio': true,
-    'supports_location_service': true,
-    'supports_vibration': true,
-  };
-  sentryDeviceJson.addAll(testUnknown);
-
   group('json', () {
+    final fixture = Fixture();
+
     test('toJson', () {
-      final json = sentryDevice.toJson();
+      final json = fixture.getSentryDeviceObject().toJson();
+      final sentryDeviceJson = fixture.getSentryDeviceJson();
 
       expect(
         MapEquality().equals(sentryDeviceJson['views'][0], json['views'][0]),
@@ -116,8 +27,18 @@ void main() {
     });
 
     test('fromJson', () {
+      final sentryDeviceJson = fixture.getSentryDeviceJson();
+
       final sentryDevice = SentryDevice.fromJson(sentryDeviceJson);
       final json = sentryDevice.toJson();
+
+      expect(
+        MapEquality().equals(sentryDeviceJson['views'][0], json['views'][0]),
+        true,
+      );
+
+      sentryDeviceJson.remove('views');
+      json.remove('views');
 
       expect(
         MapEquality().equals(sentryDeviceJson, json),
@@ -126,11 +47,34 @@ void main() {
     });
 
     test('fromJson double screen_height_pixels and screen_width_pixels', () {
-      sentryDeviceJson['screen_height_pixels'] = 100.0;
-      sentryDeviceJson['screen_width_pixels'] = 100.0;
+      final sentryDeviceJson = fixture.getSentryDeviceJson();
+      sentryDeviceJson['views'][0]['screen_height_pixels'] = 100.0;
+      sentryDeviceJson['views'][0]['screen_width_pixels'] = 100.0;
 
       final sentryDevice = SentryDevice.fromJson(sentryDeviceJson);
       final json = sentryDevice.toJson();
+
+      final data = sentryDevice;
+
+      final copy = data.copyWith();
+
+      final dataJson = data.toJson();
+      final copyJson = copy.toJson();
+
+      expect(
+        MapEquality().equals(dataJson['views'][0], copyJson['views'][0]),
+        true,
+      );
+
+      dataJson.remove('views');
+      copyJson.remove('views');
+      sentryDeviceJson.remove('views');
+      json.remove('views');
+
+      expect(
+        MapEquality().equals(dataJson, copyJson),
+        true,
+      );
 
       expect(
         MapEquality().equals(sentryDeviceJson, json),
@@ -173,8 +117,9 @@ void main() {
   });
 
   group('copyWith', () {
+    final fixture = Fixture();
     test('copyWith keeps unchanged', () {
-      final data = sentryDevice;
+      final data = fixture.getSentryDeviceObject();
 
       final copy = data.copyWith();
 
@@ -196,7 +141,7 @@ void main() {
     });
 
     test('copyWith takes new values', () {
-      final data = sentryDevice;
+      final data = fixture.getSentryDeviceObject();
 
       final bootTime = DateTime.now();
 
@@ -281,4 +226,102 @@ void main() {
       expect(false, copy.supportsVibration);
     });
   });
+}
+
+class Fixture {
+  late final testBootTime = DateTime.fromMicrosecondsSinceEpoch(0);
+
+  Map<String, dynamic> getSentryDeviceJson() {
+    var json = <String, dynamic>{
+      'name': 'testDevice',
+      'family': 'testFamily',
+      'model': 'testModel',
+      'model_id': 'testModelId',
+      'arch': 'testArch',
+      'battery_level': 23.0,
+      'manufacturer': 'testOEM',
+      'brand': 'testBrand',
+      'views': [
+        {
+          'view_id': 0,
+          'orientation': 'landscape',
+          'screen_density': 99.1,
+          'screen_dpi': 100,
+          'screen_height_pixels': 100,
+          'screen_width_pixels': 100,
+        },
+      ],
+      'online': false,
+      'charging': true,
+      'low_memory': false,
+      'simulator': true,
+      'memory_size': 1234567,
+      'free_memory': 12345,
+      'usable_memory': 9876,
+      'storage_size': 1234567,
+      'free_storage': 1234567,
+      'external_storage_size': 98765,
+      'external_free_storage': 98765,
+      'boot_time': testBootTime.toIso8601String(),
+      'battery_status': 'Unknown',
+      'cpu_description': 'M1 Pro Max Ultra',
+      'device_type': 'Flutter Device',
+      'device_unique_identifier': 'uuid',
+      'processor_count': 4,
+      'processor_frequency': 1.2,
+      'supports_accelerometer': true,
+      'supports_gyroscope': true,
+      'supports_audio': true,
+      'supports_location_service': true,
+      'supports_vibration': true,
+    };
+
+    json.addAll(testUnknown);
+    return json;
+  }
+
+  SentryDevice getSentryDeviceObject() => SentryDevice(
+        name: 'testDevice',
+        family: 'testFamily',
+        model: 'testModel',
+        modelId: 'testModelId',
+        arch: 'testArch',
+        batteryLevel: 23.0,
+        manufacturer: 'testOEM',
+        brand: 'testBrand',
+        views: [
+          SentryView(
+            0,
+            orientation: SentryOrientation.landscape,
+            screenDensity: 99.1,
+            screenDpi: 100,
+            screenHeightPixels: 100,
+            screenWidthPixels: 100,
+          )
+        ],
+        online: false,
+        charging: true,
+        lowMemory: false,
+        simulator: true,
+        memorySize: 1234567,
+        freeMemory: 12345,
+        usableMemory: 9876,
+        storageSize: 1234567,
+        freeStorage: 1234567,
+        externalStorageSize: 98765,
+        externalFreeStorage: 98765,
+        bootTime: testBootTime,
+        batteryStatus: 'Unknown',
+        cpuDescription: 'M1 Pro Max Ultra',
+        deviceType: 'Flutter Device',
+        deviceUniqueIdentifier: 'uuid',
+        processorCount: 4,
+        processorFrequency: 1.2,
+        supportsAccelerometer: true,
+        supportsGyroscope: true,
+        supportsAudio: true,
+        supportsLocationService: true,
+        supportsVibration: true,
+        unknown: testUnknown,
+      );
 }
