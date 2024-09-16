70a2a5ba9993a4de01710432d74014df5292a322
Ivan Dlugos <dlugos.ivan@gmail.com>
2024-09-16 14:23:10 +0200

    update comments

--- dart/lib/src/sentry_baggage.dart
+++ dart/lib/src/sentry_baggage.dart
@@ -55,7 +55,7 @@ class SentryBaggage {
           exception: exception,
           stackTrace: stackTrace,
         );
-        // TODO rehtow in automatedTestMode
+        // TODO rethrow in options.automatedTestMode (currently not available here to check)
       }
     }
 
