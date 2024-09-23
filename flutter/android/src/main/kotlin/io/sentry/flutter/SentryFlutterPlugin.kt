42485719f3cc6c3dc33c7acc076ef411ac1f1f31
Martin Haintz <martin.haintz@gmail.com>
2024-09-23 13:42:24 +0200

    add 60 second timeout for app starts for android native

--- flutter/android/src/main/kotlin/io/sentry/flutter/SentryFlutterPlugin.kt
+++ flutter/android/src/main/kotlin/io/sentry/flutter/SentryFlutterPlugin.kt
@@ -171,8 +171,8 @@ class SentryFlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
 
     val appStartMetrics = AppStartMetrics.getInstance()
 
-    if (!appStartMetrics.isAppLaunchedInForeground) {
-      Log.w("Sentry", "Invalid app start data: app not launched in foreground")
+    if (!appStartMetrics.isAppLaunchedInForeground || appStartMetrics.appStartTimeSpan.durationMs > 1.toDuration(DurationUnit.MINUTES)) {
+      Log.w("Sentry", "Invalid app start data: app not launched in foreground or app start took too long (>60s)")
       result.success(null)
     }
 
