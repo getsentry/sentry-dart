c174e5516e3e1d2d9070744853eb586d17e79220
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2024-10-15 16:56:30 +0200

    update

---
+++ e2e_test/flutter/android/app/src/main/kotlin/io/sentry/samples/flutter_e2e/MainActivity.kt
@@ -0,0 +1,5 @@
+package io.sentry.samples.flutter_e2e
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ e2e_test/flutter/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+
+Future<void> main() async {
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://e85b375ffb9f43cf8bdf9787768149e0@o447951.ingest.sentry.io/5428562';
+    },
+    appRunner: () => runApp(MyApp()),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter E2E',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter E2E Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            InkWell(
+                child: const Text(
+                  'Capture exception',
+                  semanticsLabel: 'Capture Exception',
+                ),
+                onTap: () {
+                  try {
+                    throw Exception('E2E test exception');
+                  } catch (exception, stacktrace) {
+                    Sentry.captureException(exception, stackTrace: stacktrace);
+                  }
+                })
+          ],
+        ),
+      ),
+    );
+  }
+}
