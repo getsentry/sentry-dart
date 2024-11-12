1e7f420ce6cda8ee65987959214dac66b217c56c
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2024-11-12 02:27:09 +0100

    update naming

--- flutter/lib/src/integrations/native_app_start_integration.dart
+++ flutter/lib/src/integrations/native_app_start_integration.dart
@@ -29,17 +29,17 @@ class NativeAppStartIntegration extends Integration<SentryFlutterOptions> {
   }
 
   final Completer<void> _appStartEndCompleter = Completer<void>();
-  bool isProcessingAppStart = false;
+  bool allowProcessing = false;
 
   @override
   void call(Hub hub, SentryFlutterOptions options) async {
     void timingsCallback(List<FrameTiming> timings) async {
-      if (isProcessingAppStart) {
+      if (allowProcessing) {
         return;
       }
       // Set immediately to prevent multiple executions
       // we only care about the first frame
-      isProcessingAppStart = true;
+      allowProcessing = true;
 
       try {
         DateTime? appStartEnd;
