0f8c6fc63e8f74aa0988161154413ffa2962f354
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2024-11-12 17:58:38 +0100

    fix compilation

--- flutter/lib/src/web/script_loader/sentry_script_loader_factory_noop.dart
+++ flutter/lib/src/web/script_loader/sentry_script_loader_factory_noop.dart
@@ -3,6 +3,12 @@ import 'sentry_script_loader.dart';
 
 SentryScriptLoader createSentryScriptLoader(
     SentryFlutterOptions options, List<Map<String, String>> scripts) {
-  throw UnsupportedError(
-      "Sentry script loader is not supported on this platform.");
+  return NoopSentryScriptLoader();
+}
+
+class NoopSentryScriptLoader implements SentryScriptLoader {
+  @override
+  Future<void> load() async {
+    // no-op
+  }
 }
