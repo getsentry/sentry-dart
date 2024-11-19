24b8ca99a1d46679b9137a13390e6e959641a352
Martin Haintz <martin.haintz@gmail.com>
2024-11-19 12:43:34 +0100

    update hive mocks

--- hive/test/mocks/mocks.mocks.dart
+++ hive/test/mocks/mocks.mocks.dart
@@ -428,21 +428,28 @@ class MockHub extends _i1.Mock implements _i2.Hub {
 
   @override
   _i5.Future<_i2.SentryId> captureTransaction(
-    _i2.SentryTransaction? transaction, {
-    _i2.SentryTraceContextHeader? traceContext,
-  }) =>
+      _i2.SentryTransaction? transaction, {
+        _i2.SentryTraceContextHeader? traceContext,
+        _i2.Hint? hint,
+      }) =>
       (super.noSuchMethod(
         Invocation.method(
           #captureTransaction,
           [transaction],
-          {#traceContext: traceContext},
+          {
+            #traceContext: traceContext,
+            #hint: hint,
+          },
         ),
         returnValue: _i5.Future<_i2.SentryId>.value(_FakeSentryId_1(
           this,
           Invocation.method(
             #captureTransaction,
             [transaction],
-            {#traceContext: traceContext},
+            {
+              #traceContext: traceContext,
+              #hint: hint,
+            },
           ),
         )),
       ) as _i5.Future<_i2.SentryId>);
