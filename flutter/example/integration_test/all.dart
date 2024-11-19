803684be5c9d2e567ca871f39bec8b18b03fa50d
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2024-11-20 00:17:04 +0100

    test alone

--- flutter/example/integration_test/all.dart
+++ flutter/example/integration_test/all.dart
@@ -1,12 +1,9 @@
 // Workaround for https://github.com/flutter/flutter/issues/101031
 import 'app_start_test.dart' as d;
-import 'integration_test.dart' as a;
-import 'profiling_test.dart' as b;
-import 'replay_test.dart' as c;
 
 void main() {
-  a.main();
-  b.main();
-  c.main();
+  // a.main();
+  // b.main();
+  // c.main();
   d.main();
 }
