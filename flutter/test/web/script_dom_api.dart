9aba0c4ffc61c2c658e9d30acca2b929ed2e7a42
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2024-11-20 16:03:56 +0100

    update

--- flutter/test/web/script_dom_api.dart
+++ flutter/test/web/script_dom_api.dart
@@ -1,5 +1,5 @@
 export 'noop_script_dom_api.dart'
-    if (dart.library.html) 'html_sentry_script_loader_test.dart'
+    if (dart.library.html) 'html_script_dom_api.dart'
     if (dart.library.js_interop) 'web_script_dom_api.dart';
 
 abstract class SentryScriptElement {
