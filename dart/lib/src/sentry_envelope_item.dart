5896109cc6d3c2e8be4e1b5175b1e2834b02151b
Denis Andrašec <denrase@gmail.com>
2024-11-26 11:02:30 +0100

    Avoid copy in `SentryEnvelopeItem.envelopeItemStream` (#2417)

--- dart/lib/src/sentry_envelope_item.dart
+++ dart/lib/src/sentry_envelope_item.dart
@@ -19,82 +19,72 @@ class SentryEnvelopeItem {
 
   /// Creates a [SentryEnvelopeItem] which sends [SentryTransaction].
   factory SentryEnvelopeItem.fromTransaction(SentryTransaction transaction) {
-    final cachedItem =
-        _CachedItem(() async => utf8JsonEncoder.convert(transaction.toJson()));
-
     final header = SentryEnvelopeItemHeader(
       SentryItemType.transaction,
-      cachedItem.getDataLength,
       contentType: 'application/json',
     );
-    return SentryEnvelopeItem(header, cachedItem.getData,
+    return SentryEnvelopeItem(
+        header, () => utf8JsonEncoder.convert(transaction.toJson()),
         originalObject: transaction);
   }
 
   factory SentryEnvelopeItem.fromAttachment(SentryAttachment attachment) {
-    final cachedItem = _CachedItem(() async => attachment.bytes);
-
     final header = SentryEnvelopeItemHeader(
       SentryItemType.attachment,
-      cachedItem.getDataLength,
       contentType: attachment.contentType,
       fileName: attachment.filename,
       attachmentType: attachment.attachmentType,
     );
-    return SentryEnvelopeItem(header, cachedItem.getData,
-        originalObject: attachment);
+    return SentryEnvelopeItem(
+      header,
+      () => attachment.bytes,
+      originalObject: attachment,
+    );
   }
 
   /// Create a [SentryEnvelopeItem] which sends [SentryUserFeedback].
   @Deprecated('Will be removed in a future version.')
   factory SentryEnvelopeItem.fromUserFeedback(SentryUserFeedback feedback) {
-    final cachedItem =
-        _CachedItem(() async => utf8JsonEncoder.convert(feedback.toJson()));
+    final dataFactory = () => utf8JsonEncoder.convert(feedback.toJson());
 
     final header = SentryEnvelopeItemHeader(
       SentryItemType.userFeedback,
-      cachedItem.getDataLength,
       contentType: 'application/json',
     );
-    return SentryEnvelopeItem(header, cachedItem.getData,
-        originalObject: feedback);
+    return SentryEnvelopeItem(
+      header,
+      dataFactory,
+      originalObject: feedback,
+    );
   }
 
   /// Create a [SentryEnvelopeItem] which holds the [SentryEvent] data.
   factory SentryEnvelopeItem.fromEvent(SentryEvent event) {
-    final cachedItem =
-        _CachedItem(() async => utf8JsonEncoder.convert(event.toJson()));
-
     return SentryEnvelopeItem(
       SentryEnvelopeItemHeader(
         event.type == 'feedback' ? 'feedback' : SentryItemType.event,
-        cachedItem.getDataLength,
         contentType: 'application/json',
       ),
-      cachedItem.getData,
+      () => utf8JsonEncoder.convert(event.toJson()),
       originalObject: event,
     );
   }
 
   /// Create a [SentryEnvelopeItem] which holds the [ClientReport] data.
   factory SentryEnvelopeItem.fromClientReport(ClientReport clientReport) {
-    final cachedItem =
-        _CachedItem(() async => utf8JsonEncoder.convert(clientReport.toJson()));
-
     return SentryEnvelopeItem(
       SentryEnvelopeItemHeader(
         SentryItemType.clientReport,
-        cachedItem.getDataLength,
         contentType: 'application/json',
       ),
-      cachedItem.getData,
+      () => utf8JsonEncoder.convert(clientReport.toJson()),
       originalObject: clientReport,
     );
   }
 
   /// Creates a [SentryEnvelopeItem] which holds several [Metric] data.
   factory SentryEnvelopeItem.fromMetrics(Map<int, Iterable<Metric>> buckets) {
-    final cachedItem = _CachedItem(() async {
+    final dataFactory = () {
       final statsd = StringBuffer();
       // Encode all metrics of a bucket in statsd format, using the bucket key,
       //  which is the timestamp of the bucket.
@@ -104,58 +94,21 @@ class SentryEnvelopeItem {
         statsd.write(encodedMetrics.join('\n'));
       }
       return utf8.encode(statsd.toString());
-    });
-
+    };
     final header = SentryEnvelopeItemHeader(
       SentryItemType.statsd,
-      cachedItem.getDataLength,
       contentType: 'application/octet-stream',
     );
-    return SentryEnvelopeItem(header, cachedItem.getData,
-        originalObject: buckets);
+    return SentryEnvelopeItem(
+      header,
+      dataFactory,
+      originalObject: buckets,
+    );
   }
 
   /// Header with info about type and length of data in bytes.
   final SentryEnvelopeItemHeader header;
 
   /// Create binary data representation of item data.
-  final Future<List<int>> Function() dataFactory;
-
-  /// Stream binary data of `Envelope` item.
-  Future<List<int>> envelopeItemStream() async {
-    // Each item needs to be encoded as one unit.
-    // Otherwise the header already got yielded if the content throws
-    // an exception.
-    try {
-      final itemHeader = utf8JsonEncoder.convert(await header.toJson());
-
-      final newLine = utf8.encode('\n');
-      final data = await dataFactory();
-      // TODO the data copy could be avoided - this would be most significant with attachments.
-      return [...itemHeader, ...newLine, ...data];
-    } catch (e) {
-      // TODO rethrow in options.automatedTestMode (currently not available here to check)
-      return [];
-    }
-  }
-}
-
-class _CachedItem {
-  _CachedItem(this._dataFactory);
-
-  final Future<List<int>> Function() _dataFactory;
-  List<int>? _data;
-
-  Future<List<int>> getData() async {
-    _data ??= await _dataFactory();
-    return _data!;
-  }
-
-  Future<int> getDataLength() async {
-    try {
-      return (await getData()).length;
-    } catch (_) {
-      return -1;
-    }
-  }
+  final FutureOr<List<int>> Function() dataFactory;
 }
