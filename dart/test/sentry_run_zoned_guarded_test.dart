705a12e736ad5ce5408828b585d283532ba0277f
Denis Andrasec <denrase@gmail.com>
2024-12-03 17:20:34 +0100

    fix import and changelog

--- dart/test/sentry_run_zoned_guarded_test.dart
+++ dart/test/sentry_run_zoned_guarded_test.dart
@@ -4,6 +4,7 @@ library dart_test;
 import 'dart:async';
 
 import 'package:sentry/sentry.dart';
+import 'package:sentry/src/sentry_run_zoned_guarded.dart';
 import 'package:test/test.dart';
 
 import 'mocks/mock_hub.dart';
