7f02fa2cb0a0627c6bbf24e5625ad4f1ef6912da
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2024-12-10 13:44:31 +0100

    remove fn

--- flutter/lib/src/web/script_loader/web_script_dom_api.dart
+++ flutter/lib/src/web/script_loader/web_script_dom_api.dart
@@ -53,12 +53,3 @@ Future<void> loadScript(String src, SentryOptions options,
   }
   return completer.future;
 }
-
-void injectMetaTag(Map<String, String> attributes) {
-  final HTMLMetaElement meta =
-      document.createElement('meta') as HTMLMetaElement;
-  for (final MapEntry<String, String> attribute in attributes.entries) {
-    meta.setAttribute(attribute.key, attribute.value);
-  }
-  document.head!.appendChild(meta);
-}
