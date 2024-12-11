ef100b659d59d949312f39b61583f71fbc350ac4
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2024-12-11 13:28:51 +0100

    uncomment other tests

--- flutter/example/integration_test/integration_test.dart
+++ flutter/example/integration_test/integration_test.dart
@@ -44,126 +44,126 @@ void main() {
 
   // Tests
 
-  // testWidgets('setup sentry and render app', (tester) async {
-  //   await setupSentryAndApp(tester);
-  //
-  //   // Find any UI element and verify it is present.
-  //   expect(find.text('Open another Scaffold'), findsOneWidget);
-  // });
-  //
-  // testWidgets('setup sentry and capture event', (tester) async {
-  //   await setupSentryAndApp(tester);
-  //
-  //   final event = SentryEvent();
-  //   final sentryId = await Sentry.captureEvent(event);
-  //
-  //   expect(sentryId != const SentryId.empty(), true);
-  // });
-  //
-  // testWidgets('setup sentry and capture exception', (tester) async {
-  //   await setupSentryAndApp(tester);
-  //
-  //   try {
-  //     throw const SentryException(
-  //         type: 'StarError', value: 'I have a bad feeling about this...');
-  //   } catch (exception, stacktrace) {
-  //     final sentryId =
-  //         await Sentry.captureException(exception, stackTrace: stacktrace);
-  //
-  //     expect(sentryId != const SentryId.empty(), true);
-  //   }
-  // });
-  //
-  // testWidgets('setup sentry and capture message', (tester) async {
-  //   await setupSentryAndApp(tester);
-  //
-  //   final sentryId = await Sentry.captureMessage('hello world!');
-  //
-  //   expect(sentryId != const SentryId.empty(), true);
-  // });
-  //
-  // testWidgets('setup sentry and capture user feedback', (tester) async {
-  //   await setupSentryAndApp(tester);
-  //
-  //   // ignore: deprecated_member_use_from_same_package
-  //   // ignore: deprecated_member_use
-  //   final feedback = SentryUserFeedback(
-  //       eventId: SentryId.newId(),
-  //       name: 'fixture-name',
-  //       email: 'fixture@email.com',
-  //       comments: 'fixture-comments');
-  //   // ignore: deprecated_member_use
-  //   await Sentry.captureUserFeedback(feedback);
-  // });
-  //
-  // testWidgets('setup sentry and capture feedback', (tester) async {
-  //   await setupSentryAndApp(tester);
-  //
-  //   // ignore: deprecated_member_use_from_same_package
-  //   // ignore: deprecated_member_use
-  //   final associatedEventId = await Sentry.captureMessage('Associated');
-  //   final feedback = SentryFeedback(
-  //     message: 'message',
-  //     contactEmail: 'john.appleseed@apple.com',
-  //     name: 'John Appleseed',
-  //     associatedEventId: associatedEventId,
-  //   );
-  //   await Sentry.captureFeedback(feedback);
-  // });
-  //
-  // testWidgets('setup sentry and close', (tester) async {
-  //   await setupSentryAndApp(tester);
-  //
-  //   await Sentry.close();
-  // });
-  //
-  // testWidgets('setup sentry and add breadcrumb', (tester) async {
-  //   await setupSentryAndApp(tester);
-  //
-  //   final breadcrumb = Breadcrumb(message: 'fixture-message');
-  //   await Sentry.addBreadcrumb(breadcrumb);
-  // });
-  //
-  // testWidgets('setup sentry and configure scope', (tester) async {
-  //   await setupSentryAndApp(tester);
-  //
-  //   await Sentry.configureScope((scope) async {
-  //     await scope.setContexts('contexts-key', 'contexts-value');
-  //     await scope.removeContexts('contexts-key');
-  //
-  //     final user = SentryUser(id: 'fixture-id');
-  //     await scope.setUser(user);
-  //     await scope.setUser(null);
-  //
-  //     final breadcrumb = Breadcrumb(message: 'fixture-message');
-  //     await scope.addBreadcrumb(breadcrumb);
-  //     await scope.clearBreadcrumbs();
-  //
-  //     // ignore: deprecated_member_use
-  //     await scope.setExtra('extra-key', 'extra-value');
-  //     // ignore: deprecated_member_use
-  //     await scope.removeExtra('extra-key');
-  //
-  //     await scope.setTag('tag-key', 'tag-value');
-  //     await scope.removeTag('tag-key');
-  //   });
-  // });
-  //
-  // testWidgets('setup sentry and start transaction', (tester) async {
-  //   await setupSentryAndApp(tester);
-  //
-  //   final transaction = Sentry.startTransaction('transaction', 'test');
-  //   await transaction.finish();
-  // });
-  //
-  // testWidgets('setup sentry and start transaction with context',
-  //     (tester) async {
-  //   await setupSentryAndApp(tester);
-  //
-  //   final context = SentryTransactionContext('transaction', 'test');
-  //   final transaction = Sentry.startTransactionWithContext(context);
-  //   await transaction.finish();
-  // });
+  testWidgets('setup sentry and render app', (tester) async {
+    await setupSentryAndApp(tester);
+
+    // Find any UI element and verify it is present.
+    expect(find.text('Open another Scaffold'), findsOneWidget);
+  });
+
+  testWidgets('setup sentry and capture event', (tester) async {
+    await setupSentryAndApp(tester);
+
+    final event = SentryEvent();
+    final sentryId = await Sentry.captureEvent(event);
+
+    expect(sentryId != const SentryId.empty(), true);
+  });
+
+  testWidgets('setup sentry and capture exception', (tester) async {
+    await setupSentryAndApp(tester);
+
+    try {
+      throw const SentryException(
+          type: 'StarError', value: 'I have a bad feeling about this...');
+    } catch (exception, stacktrace) {
+      final sentryId =
+          await Sentry.captureException(exception, stackTrace: stacktrace);
+
+      expect(sentryId != const SentryId.empty(), true);
+    }
+  });
+
+  testWidgets('setup sentry and capture message', (tester) async {
+    await setupSentryAndApp(tester);
+
+    final sentryId = await Sentry.captureMessage('hello world!');
+
+    expect(sentryId != const SentryId.empty(), true);
+  });
+
+  testWidgets('setup sentry and capture user feedback', (tester) async {
+    await setupSentryAndApp(tester);
+
+    // ignore: deprecated_member_use_from_same_package
+    // ignore: deprecated_member_use
+    final feedback = SentryUserFeedback(
+        eventId: SentryId.newId(),
+        name: 'fixture-name',
+        email: 'fixture@email.com',
+        comments: 'fixture-comments');
+    // ignore: deprecated_member_use
+    await Sentry.captureUserFeedback(feedback);
+  });
+
+  testWidgets('setup sentry and capture feedback', (tester) async {
+    await setupSentryAndApp(tester);
+
+    // ignore: deprecated_member_use_from_same_package
+    // ignore: deprecated_member_use
+    final associatedEventId = await Sentry.captureMessage('Associated');
+    final feedback = SentryFeedback(
+      message: 'message',
+      contactEmail: 'john.appleseed@apple.com',
+      name: 'John Appleseed',
+      associatedEventId: associatedEventId,
+    );
+    await Sentry.captureFeedback(feedback);
+  });
+
+  testWidgets('setup sentry and close', (tester) async {
+    await setupSentryAndApp(tester);
+
+    await Sentry.close();
+  });
+
+  testWidgets('setup sentry and add breadcrumb', (tester) async {
+    await setupSentryAndApp(tester);
+
+    final breadcrumb = Breadcrumb(message: 'fixture-message');
+    await Sentry.addBreadcrumb(breadcrumb);
+  });
+
+  testWidgets('setup sentry and configure scope', (tester) async {
+    await setupSentryAndApp(tester);
+
+    await Sentry.configureScope((scope) async {
+      await scope.setContexts('contexts-key', 'contexts-value');
+      await scope.removeContexts('contexts-key');
+
+      final user = SentryUser(id: 'fixture-id');
+      await scope.setUser(user);
+      await scope.setUser(null);
+
+      final breadcrumb = Breadcrumb(message: 'fixture-message');
+      await scope.addBreadcrumb(breadcrumb);
+      await scope.clearBreadcrumbs();
+
+      // ignore: deprecated_member_use
+      await scope.setExtra('extra-key', 'extra-value');
+      // ignore: deprecated_member_use
+      await scope.removeExtra('extra-key');
+
+      await scope.setTag('tag-key', 'tag-value');
+      await scope.removeTag('tag-key');
+    });
+  });
+
+  testWidgets('setup sentry and start transaction', (tester) async {
+    await setupSentryAndApp(tester);
+
+    final transaction = Sentry.startTransaction('transaction', 'test');
+    await transaction.finish();
+  });
+
+  testWidgets('setup sentry and start transaction with context',
+      (tester) async {
+    await setupSentryAndApp(tester);
+
+    final context = SentryTransactionContext('transaction', 'test');
+    final transaction = Sentry.startTransactionWithContext(context);
+    await transaction.finish();
+  });
 
   group('e2e', () {
     var output = find.byKey(const Key('output'));
