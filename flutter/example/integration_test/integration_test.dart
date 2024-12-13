4f83566c35373ad19eb65d2e02bc6559ea7ab772
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2024-12-13 14:24:05 +0100

    cleanup

--- flutter/example/integration_test/integration_test.dart
+++ flutter/example/integration_test/integration_test.dart
@@ -6,7 +6,8 @@ import 'dart:convert';
 
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/httpt';
+import 'package:http/http.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:sentry_flutter_example/main.dart';
 
 void main() {
