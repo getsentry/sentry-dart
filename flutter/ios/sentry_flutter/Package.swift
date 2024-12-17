b1b32ca745a0935df0ca52a887ad08d07a66c447
Denis Andrasec <denrase@gmail.com>
2024-12-17 14:18:38 +0100

    Bump to release version

--- flutter/ios/sentry_flutter/Package.swift
+++ flutter/ios/sentry_flutter/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "sentry-flutter", targets: ["sentry_flutter", "sentry_flutter_objc"])
     ],
     dependencies: [
-      .package(url: "https://github.com/getsentry/sentry-cocoa", from: "8.42.0-beta.2")
+      .package(url: "https://github.com/getsentry/sentry-cocoa", from: "8.42.0")
     ],
     targets: [
         .target(
