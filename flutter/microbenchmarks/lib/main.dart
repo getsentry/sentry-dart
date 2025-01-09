bb7fd77c7af03567bbd92b5df4315132689fb0b8
Ivan Dlugos <dlugos.ivan@gmail.com>
2025-01-09 21:09:43 +0100

    memory benchmarks

--- flutter/microbenchmarks/lib/main.dart
+++ flutter/microbenchmarks/lib/main.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 import 'src/image_bench.dart' as image_bench;
+import 'src/memory_bench.dart' as memory_bench;
 
 Future<void> main() async {
   await image_bench.execute();
+  await memory_bench.execute();
   print('Benchmarks finished');
   exit(0);
 }
