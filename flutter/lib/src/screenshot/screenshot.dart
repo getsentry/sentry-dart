05574bb4e8fee4d900a82409e1bab409ab593bd1
Ivan Dlugos <dlugos.ivan@gmail.com>
2025-01-10 10:03:51 +0100

    improve screenshot comparison performance

--- flutter/lib/src/screenshot/screenshot.dart
+++ flutter/lib/src/screenshot/screenshot.dart
@@ -1,9 +1,6 @@
 import 'dart:async';
 import 'dart:developer';
-import 'dart:ffi';
 import 'dart:ui';
-// ignore: unnecessary_import // backcompatibility for Flutter < 3.3
-import 'dart:typed_data';
 
 import 'package:flutter/foundation.dart';
 import 'package:meta/meta.dart';
@@ -24,16 +21,21 @@ class Screenshot {
   int get height => _image.height;
 
   Future<ByteData> get rawData {
-    _rawData ??= _image
-        .toByteData(format: ImageByteFormat.rawUnmodified)
-        .then((data) => data!);
+    _rawData ??= _encode(ImageByteFormat.rawUnmodified);
     return _rawData!;
   }
 
   Future<ByteData> get pngData {
-    _pngData ??=
-        _image.toByteData(format: ImageByteFormat.png).then((data) => data!);
+    _pngData ??= _encode(ImageByteFormat.png);
     return _pngData!;
+  }
+
+  Future<ByteData> _encode(ImageByteFormat format) async {
+    Timeline.startSync('Sentry::screenshotTo${format.name}', flow: flow);
+    final result =
+        await _image.toByteData(format: format).then((data) => data!);
+    Timeline.finishSync();
+    return result;
   }
 
   Future<bool> hasSameImageAs(Screenshot other) async {
@@ -41,33 +43,7 @@ class Screenshot {
       return false;
     }
 
-    final thisData = await rawData;
-    final otherData = await other.rawData;
-    if (thisData.lengthInBytes != otherData.lengthInBytes) {
-      return false;
-    }
-    if (identical(thisData, otherData)) {
-      return true;
-    }
-
-    // Note: listEquals() is slow because it compares each byte pair.
-    // Ideally, we would use memcmp with Uint8List.address but that's only
-    // available since Dart 3.5.0.
-    // For now, the best we can do is compare by chunks
-    var pos = 0;
-    while (pos + 8 < thisData.lengthInBytes) {
-      if (thisData.getInt64(pos) != otherData.getInt64(pos)) {
-        return false;
-      }
-      pos += 8;
-    }
-    while (pos < thisData.lengthInBytes) {
-      if (thisData.getUint8(pos) != otherData.getUint8(pos)) {
-        return false;
-      }
-      pos++;
-    }
-    return true;
+    return listEquals(await rawData, await other.rawData);
   }
 
   Screenshot clone() {
@@ -77,4 +53,39 @@ class Screenshot {
   }
 
   void dispose() => _image.dispose();
+
+  /// Efficiently compares two memory regions for data equality..
+  /// Ideally, we would use memcmp with Uint8List.address but that's only
+  /// available since Dart 3.5.0.
+  /// For now, the best we can do is compare by chunks of 8 bytes.
+  @visibleForTesting
+  static bool listEquals(ByteData dataA, ByteData dataB) {
+    if (identical(dataA, dataB)) {
+      return true;
+    }
+    if (dataA.lengthInBytes != dataB.lengthInBytes) {
+      return false;
+    }
+
+    final numWords = dataA.lengthInBytes ~/ 8;
+    final wordsA = dataA.buffer.asUint64List(0, numWords);
+    final wordsB = dataB.buffer.asUint64List(0, numWords);
+
+    for (var i = 0; i < wordsA.length; i++) {
+      if (wordsA[i] != wordsB[i]) {
+        return false;
+      }
+    }
+
+    // Compare any remaining bytes.
+    final bytesA = dataA.buffer.asUint8List(wordsA.lengthInBytes);
+    final bytesB = dataA.buffer.asUint8List(wordsA.lengthInBytes);
+    for (var i = 0; i < bytesA.lengthInBytes; i++) {
+      if (bytesA[i] != bytesB[i]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
