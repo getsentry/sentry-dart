fa3c195664936201e00d47bee3d74359f964a0e1
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2025-01-29 16:12:11 +0100

    migrate flutter android to use declarative plugins block

--- flutter/example/android/app/src/main/cpp/native-sample.cpp
+++ flutter/example/android/app/src/main/cpp/native-sample.cpp
@@ -13,11 +13,10 @@ JNIEXPORT void JNICALL Java_io_sentry_samples_flutter_MainActivity_crash(JNIEnv 
 }
 
 JNIEXPORT void JNICALL Java_io_sentry_samples_flutter_MainActivity_message(JNIEnv *env, jclass cls) {
-    // __android_log_print(ANDROID_LOG_WARN, TAG, "Sending message.");
     sentry_value_t event = sentry_value_new_message_event(
-            /*   level */ SENTRY_LEVEL_INFO,
-            /*  logger */ "native",
-            /* message */ "message from C++!"
+            SENTRY_LEVEL_INFO,
+            "native",
+            "message from C++!"
     );
     sentry_capture_event(event);
 }
