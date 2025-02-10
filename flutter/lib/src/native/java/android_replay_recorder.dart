f17c72deee98ccdb099e926d719a6db307639987
Ivan Dlugos <dlugos.ivan@gmail.com>
2025-02-10 11:09:21 +0100

    use persistent isolate

--- flutter/lib/src/native/java/android_replay_recorder.dart
+++ flutter/lib/src/native/java/android_replay_recorder.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:isolate';
 
 import 'package:jni/jni.dart';
@@ -10,19 +11,30 @@ import 'binding.dart' as native;
 
 @internal
 class AndroidReplayRecorder extends ScheduledScreenshotRecorder {
-  late final _nativeReplay = native.SentryFlutterPlugin$Companion(null)
-      .privateSentryGetReplayIntegration()!;
-  // Android Bitmap creation is a bit costly so we reuse it between captures.
-  native.Bitmap? _bitmap;
+  _AndroidNativeReplayWorker? _worker;
 
   AndroidReplayRecorder(super.config, super.options) {
     super.callback = _addReplayScreenshot;
   }
 
+  @override
+  Future<void> start() async {
+    final spawningWorker = _AndroidNativeReplayWorker.spawn();
+    super.start();
+    _worker = await spawningWorker;
+  }
+
+  @override
+  Future<void> stop() async {
+    await super.stop();
+    _worker?.close();
+    _worker = null;
+  }
+
   Future<void> _addReplayScreenshot(
       Screenshot screenshot, bool isNewlyCaptured) async {
     // TODO this is currently missing in native onScreenshotRecorded()
-    // final timestamp = screenshot.timestamp.millisecondsSinceEpoch;
+    final timestamp = screenshot.timestamp.millisecondsSinceEpoch;
 
     try {
       final data = await screenshot.rawRgbaData;
@@ -37,10 +49,11 @@ class AndroidReplayRecorder extends ScheduledScreenshotRecorder {
       int width = screenshot.width;
       int height = screenshot.height;
 
-      // Note: possible future improvement: long-lived isolate
-      await Isolate.run(
-          () => _addReplayScreenshotNative(jBuffer, width, height),
-          debugName: 'SentryReplayRecorder');
+      await _worker!.nativeAddScreenshot(_WorkItem(
+          timestamp: timestamp,
+          jBuffer: jBuffer,
+          width: width,
+          height: height));
     } catch (error, stackTrace) {
       options.logger(
         SentryLevel.error,
@@ -53,35 +66,142 @@ class AndroidReplayRecorder extends ScheduledScreenshotRecorder {
       }
     }
   }
+}
 
-  @override
-  Future<void> stop() async {
-    await super.stop();
-    _bitmap?.release();
-    _bitmap = null;
-  }
+// Based on https://dart.dev/language/isolates#robust-ports-example
+class _AndroidNativeReplayWorker {
+  final SendPort _commands;
+  final ReceivePort _responses;
+  final Map<int, Completer<Object?>> _activeRequests = {};
+  int _idCounter = 0;
+  bool _closed = false;
 
-  void _addReplayScreenshotNative(JByteBuffer jBuffer, int width, int height) {
-    if (_bitmap != null) {
-      if (_bitmap!.getWidth() != width || _bitmap!.getHeight() != height) {
-        _bitmap!.release();
-        _bitmap = null;
-      }
-    }
+  static Future<_AndroidNativeReplayWorker> spawn() async {
+    // Create a receive port and add its initial message handler
+    final initPort = RawReceivePort();
+    final connection = Completer<(ReceivePort, SendPort)>.sync();
+    initPort.handler = (SendPort commandPort) {
+      connection.complete((
+        ReceivePort.fromRawReceivePort(initPort),
+        commandPort,
+      ));
+    };
 
-    // https://developer.android.com/reference/android/graphics/Bitmap#createBitmap(int,%20int,%20android.graphics.Bitmap.Config)
-    // Note: in the currently generated API this may return null so we null-check below.
-    _bitmap ??= native.Bitmap.createBitmap$3(
-        width, height, native.Bitmap$Config.ARGB_8888);
-
+    // Spawn the isolate.
     try {
-      _bitmap?.copyPixelsFromBuffer(jBuffer);
-    } finally {
-      jBuffer.release();
+      await Isolate.spawn(_startRemoteIsolate, (initPort.sendPort),
+          debugName: 'SentryReplayRecorder');
+    } on Object {
+      initPort.close();
+      rethrow;
     }
 
-    if (_bitmap != null) {
-      _nativeReplay.onScreenshotRecorded(_bitmap!);
+    final (ReceivePort receivePort, SendPort sendPort) =
+        await connection.future;
+
+    return _AndroidNativeReplayWorker._(receivePort, sendPort);
+  }
+
+  _AndroidNativeReplayWorker._(this._responses, this._commands) {
+    _responses.listen(_handleResponsesFromIsolate);
+  }
+
+  Future<Object?> nativeAddScreenshot(_WorkItem item) async {
+    if (_closed) throw StateError('Closed');
+    final completer = Completer<Object?>.sync();
+    final id = _idCounter++;
+    _activeRequests[id] = completer;
+    _commands.send((id, item));
+    return await completer.future;
+  }
+
+  void _handleResponsesFromIsolate(dynamic message) {
+    final (int id, Object? response) = message as (int, Object?);
+    final completer = _activeRequests.remove(id)!;
+
+    if (response is RemoteError) {
+      completer.completeError(response);
+    } else {
+      completer.complete(response);
+    }
+
+    if (_closed && _activeRequests.isEmpty) _responses.close();
+  }
+
+  /// This is the actual Android native implementation, the rest is just plumbing.
+  static void _handleCommandsToIsolate(
+    ReceivePort receivePort,
+    SendPort sendPort,
+  ) {
+    // Android Bitmap creation is a bit costly so we reuse it between captures.
+    native.Bitmap? bitmap;
+
+    final _nativeReplay = native.SentryFlutterPlugin$Companion(null)
+        .privateSentryGetReplayIntegration()!;
+
+    receivePort.listen((message) {
+      if (message == 'shutdown') {
+        receivePort.close();
+        return;
+      }
+      final (id, item) = message as (int, _WorkItem);
+      try {
+        if (bitmap != null) {
+          if (bitmap!.getWidth() != item.width ||
+              bitmap!.getHeight() != item.height) {
+            bitmap!.release();
+            bitmap = null;
+          }
+        }
+
+        // https://developer.android.com/reference/android/graphics/Bitmap#createBitmap(int,%20int,%20android.graphics.Bitmap.Config)
+        // Note: in the currently generated API this may return null so we null-check below.
+        bitmap ??= native.Bitmap.createBitmap$3(
+            item.width, item.height, native.Bitmap$Config.ARGB_8888);
+
+        try {
+          bitmap?.copyPixelsFromBuffer(item.jBuffer);
+        } finally {
+          item.jBuffer.release();
+        }
+
+        if (bitmap != null) {
+          _nativeReplay.onScreenshotRecorded(bitmap!);
+        }
+
+        sendPort.send((id, null));
+      } catch (e, stacktrace) {
+        sendPort.send((id, RemoteError(e.toString(), stacktrace.toString())));
+      }
+    });
+  }
+
+  static void _startRemoteIsolate(SendPort sendPort) {
+    final receivePort = ReceivePort();
+    sendPort.send(receivePort.sendPort);
+    _handleCommandsToIsolate(receivePort, sendPort);
+  }
+
+  void close() {
+    if (!_closed) {
+      _closed = true;
+      _commands.send('shutdown');
+      if (_activeRequests.isEmpty) _responses.close();
+      print('--- port closed --- ');
     }
   }
+}
+
+class _WorkItem {
+  final int timestamp;
+  final JByteBuffer jBuffer;
+  final int width;
+  final int height;
+
+  const _WorkItem({
+    required this.timestamp,
+    required this.jBuffer,
+    required this.width,
+    required this.height,
+  });
 }
--- flutter/lib/src/native/java/sentry_native_java.dart
+++ flutter/lib/src/native/java/sentry_native_java.dart
@@ -29,7 +29,8 @@ class SentryNativeJava extends SentryNativeChannel {
                 height: (call.arguments['height'] as num).toDouble(),
                 frameRate: call.arguments['frameRate'] as int);
 
-            _replayRecorder = AndroidReplayRecorder(config, options)..start();
+            _replayRecorder = AndroidReplayRecorder(config, options);
+            await _replayRecorder!.start();
 
             hub.configureScope((s) {
               // ignore: invalid_use_of_internal_member
