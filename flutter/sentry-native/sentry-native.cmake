10a95324b0687337a3a067dbe0aa2d94e1093f1a
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2025-03-13 21:14:01 +0100

    update

--- flutter/sentry-native/sentry-native.cmake
+++ flutter/sentry-native/sentry-native.cmake
@@ -4,18 +4,12 @@ message(STATUS "Fetching Sentry native version: ${SENTRY_NATIVE_version} from ${
 set(SENTRY_SDK_NAME "sentry.native.flutter" CACHE STRING "The SDK name to report when sending events." FORCE)
 set(SENTRY_BUILD_SHARED_LIBS ON CACHE BOOL "Build shared libraries (.dll/.so) instead of static ones (.lib/.a)" FORCE)
 
-# Note: the backend is also set in linux/CMakeLists.txt and windows/CMakeLists.txt. This overwrites those if user sets an env var.
-if("$ENV{SENTRY_NATIVE_BACKEND}" STREQUAL "")
-    # Windows ARM64 currently has issues with crashpad so we opt for using breakpad
-    # See: https://github.com/getsentry/sentry-dart/issues/2498
-    if(WIN32 AND CMAKE_SYSTEM_PROCESSOR STREQUAL "ARM64")
-        set(SENTRY_BACKEND "breakpad" CACHE STRING "The sentry backend responsible for reporting crashes" FORCE)
-    else()
-        set(SENTRY_BACKEND "crashpad" CACHE STRING "The sentry backend responsible for reporting crashes" FORCE)
-    endif()
-else()
+if(NOT "$ENV{SENTRY_NATIVE_BACKEND}" STREQUAL "")
+    # If environment variable is set, it takes highest precedence
     set(SENTRY_BACKEND $ENV{SENTRY_NATIVE_BACKEND} CACHE STRING "The sentry backend responsible for reporting crashes" FORCE)
 endif()
+
+message(STATUS "Sentry Native is using backend: ${SENTRY_BACKEND}")
 
 include(FetchContent)
 FetchContent_Declare(
