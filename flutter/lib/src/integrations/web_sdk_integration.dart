433833142bf3373c1fd891ea97cf2db63b3565bb
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2025-03-14 11:30:54 +0100

    remove unnecessary code

--- flutter/lib/src/integrations/web_sdk_integration.dart
+++ flutter/lib/src/integrations/web_sdk_integration.dart
@@ -52,52 +52,6 @@ class WebSdkIntegration implements Integration<SentryFlutterOptions> {
     }
   }
 
-  // Currently updating sessions is only relevant for web
-  // iOS & Android sessions are handled by the native SDKs directly
-  void _updateSessionFromEvent(SentryEvent event) async {
-    if (_options?.enableAutoSessionTracking == false) {
-      _options?.logger(
-        SentryLevel.info,
-        'Auto session tracking is disabled. ',
-      );
-      return;
-    }
-
-    bool crashed = event.level == SentryLevel.fatal;
-    bool errored = false;
-
-    if (event.exceptions?.isNotEmpty == true) {
-      errored = true;
-
-      for (final exception in event.exceptions!) {
-        if (exception.mechanism?.handled == false) {
-          crashed = true;
-          break;
-        }
-      }
-    }
-
-    // A session is updated and that session update is sent in only one of the two following scenarios:
-    // 1. Session with non terminal status and 0 errors + an error occurred -> Will set error count to 1 and send update
-    // 2. Session with non terminal status and 1 error + a crash occurred -> Will set status crashed and send update
-    final session = await _web.getSession();
-    if (session == null) {
-      return;
-    }
-
-    final sessionNonTerminal = session['status'] == 'ok';
-    final shouldUpdateAndSend =
-        (sessionNonTerminal && session['errors'] == 0) ||
-            (sessionNonTerminal && crashed);
-
-    if (shouldUpdateAndSend) {
-      final status = crashed ? 'crashed' : session['status'].toString();
-      await _web.updateSession(
-          status: status, errors: errored || crashed ? 1 : 0);
-      await _web.captureSession();
-    }
-  }
-
   @override
   FutureOr<void> close() async {
     try {
