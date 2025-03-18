92a6c633776a56d38bfeaf290dd28d011ea3b5ef
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2025-03-18 12:13:51 +0100

    update dart comment

--- dart/lib/src/observers.dart
+++ dart/lib/src/observers.dart
@@ -7,6 +7,8 @@ import '../sentry.dart';
 // This file will contain observer definitions that are executed during
 // specific points in the SDK such as as right before an event is sent.
 // Only for internal use, e.g updating sessions only when an event is fully processed.
+// Not to be confused with the public callbacks such as beforeSend.
+// These should not mutate the data that is passed through the observer.
 
 /// Called right before an event is sent, after all processing is complete.
 /// Should not modify the event at this point.
