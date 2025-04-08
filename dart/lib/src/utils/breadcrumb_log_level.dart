f766e3c5ecc695fa31992be67d0b134b34c94723
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2025-04-08 14:53:50 +0200

    Be able to return null from getBreadcrumbLogLevelFromHttpStatusCode

--- dart/lib/src/utils/breadcrumb_log_level.dart
+++ dart/lib/src/utils/breadcrumb_log_level.dart
@@ -4,13 +4,13 @@ import '../../sentry.dart';
 
 /// Determine a breadcrumb's log level (only `warning` or `error`) based on an HTTP status code.
 @internal
-SentryLevel getBreadcrumbLogLevelFromHttpStatusCode(int statusCode) {
+SentryLevel? getBreadcrumbLogLevelFromHttpStatusCode(int statusCode) {
   // NOTE: null defaults to 'info' in Sentry
   if (statusCode >= 400 && statusCode < 500) {
     return SentryLevel.warning;
   } else if (statusCode >= 500) {
     return SentryLevel.error;
   } else {
-    return SentryLevel.info;
+    return null;
   }
 }
