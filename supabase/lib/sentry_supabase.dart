30c089b14554ccc2771854ca54ea507362481d9b
Denis Andrasec <denrase@gmail.com>
2025-04-22 17:51:08 +0200

    add to flutter/example app

--- supabase/lib/sentry_supabase.dart
+++ supabase/lib/sentry_supabase.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library;
 
-export 'src/sentry_supabase.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/sentry_supabase_client.dart';
