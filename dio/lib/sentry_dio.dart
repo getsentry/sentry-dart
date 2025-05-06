9fbcf89f200f3ce1a1240abd7e02cf71b160b455
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2025-05-06 09:10:32 +0200

    refactor(export): add sentry transformer to `sentry_dio` exports

--- dio/lib/sentry_dio.dart
+++ dio/lib/sentry_dio.dart
@@ -5,3 +5,6 @@ export 'src/sentry_dio_extension.dart';
 // Export the processor in case people want to use it standalone.
 // Normally one doesn't need to use it directly.
 export 'src/dio_event_processor.dart';
+
+// Export the transformer in case people want to use it standalone.
+export 'src/sentry_transformer.dart';
