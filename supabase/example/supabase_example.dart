cb7f1f59d4cccff40def49091e218b3d92fada2f
Denis Andrasec <denrase@gmail.com>
2025-05-21 14:40:11 +0200

    update example

--- supabase/example/supabase_example.dart
+++ supabase/example/supabase_example.dart
@@ -1,6 +1,18 @@
-import 'package:supabase/sentry_supabase.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:sentry_supabase/sentry_supabase.dart';
 
-void main() {
-  final client = SentrySupabaseClient();
-  // TODO: Add supabase instumentation sample
-}
+// Create a [SentrySupabaseClient] and pass it to Supabase during initialization.
+
+final sentrySupabaseClient = SentrySupabaseClient();
+await Supabase.initialize(
+  url: '<YOUR_SUPABASE_URL>',
+  anonKey: '<YOUR_SUPABASE_ANON_KEY>',
+  httpClient: sentrySupabaseClient,
+);
+
+// Now all [Supabase] operations and queries will
+// be instrumented with Sentry breadcrumbs, traces and errors.
+
+final issues = await Supabase.instance.client
+  .from('issues')
+  .select();
