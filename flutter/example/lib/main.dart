e4d1bf6d5e311cffb257340e5dd7bb57fe827886
Denis Andrasec <denrase@gmail.com>
2025-05-21 16:00:41 +0200

    format main

--- flutter/example/lib/main.dart
+++ flutter/example/lib/main.dart
@@ -42,8 +42,6 @@ var _isIntegrationTest = false;
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 Future<void> main() async {
-  
-
   await setupSentry(
     () => runApp(
       SentryWidget(
