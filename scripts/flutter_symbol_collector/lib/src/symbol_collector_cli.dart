14a6e0770bffe520242c35609800693fa86848fc
GitHub <noreply@github.com>
2025-06-12 09:47:04 +0000

    chore: update scripts/update-symbol-collector.sh to 2.1.0

--- scripts/flutter_symbol_collector/lib/src/symbol_collector_cli.dart
+++ scripts/flutter_symbol_collector/lib/src/symbol_collector_cli.dart
@@ -19,7 +19,7 @@ class SymbolCollectorCli {
 
   // https://github.com/getsentry/symbol-collector/releases
   @internal
-  static const version = '1.23.0';
+  static const version = '2.1.0';
 
   @internal
   late final String cli;
