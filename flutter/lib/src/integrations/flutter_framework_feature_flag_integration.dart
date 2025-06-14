e91d49262cd36a1b78ebfd84b95c8c7a735080b0
Jonas Uekoetter <jonas.uekotter@wolt.com>
2025-06-14 18:15:23 +0200

    explanation how that works

--- flutter/lib/src/integrations/flutter_framework_feature_flag_integration.dart
+++ flutter/lib/src/integrations/flutter_framework_feature_flag_integration.dart
@@ -2,8 +2,13 @@ import 'dart:async';
 
 import 'package:sentry/sentry.dart';
 
-// See the following PS for the introduction
-// https://github.com/flutter/flutter/pull/168437
+// The Flutter framework feature flag works like this:
+// An enabled (experimental) feature gets added to the `FLUTTER_ENABLED_FEATURE_FLAGS`
+// dart define. Being in there means the feature is enabled, the feature is disabled
+// if it's not in there.
+// As a result, we also don't know the whole list of flags, but only the active ones.
+//
+// See https://github.com/flutter/flutter/pull/168437
 class FlutterFrameworkFeatureFlagIntegration extends Integration<SentryOptions> {
   @override
   FutureOr<void> call(Hub hub, SentryOptions options) {
@@ -20,7 +25,7 @@ class FlutterFrameworkFeatureFlagIntegration extends Integration<SentryOptions> 
 }
 
 extension FlutterFrameworkFeatureFlagIntegrationX on List<Integration<SentryOptions>> {
-  /// For better tree-shake-ability we only add the integration if a feature flag is enabled.
+  /// For better tree-shake-ability we only add the integration if any feature flag is enabled.
   void addFlutterFrameworkFeatureFlagIntegration() {
     if(const bool.hasEnvironment('FLUTTER_ENABLED_FEATURE_FLAGS')) {
       add(FlutterFrameworkFeatureFlagIntegration());
