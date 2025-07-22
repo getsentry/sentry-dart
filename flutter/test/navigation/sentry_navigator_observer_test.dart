e585f0e3bb171dbd0f6cd63dc30c320c74b40171
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2025-07-22 13:16:22 +0200

    Fix analyze

--- flutter/test/navigation/sentry_navigator_observer_test.dart
+++ flutter/test/navigation/sentry_navigator_observer_test.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:platform/platform.dart';
 import 'package:sentry/src/platform/mock_platform.dart';
 import 'package:sentry/src/sentry_tracer.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
