481ddd574fcf194bd395ff7a869ea5d274b098f0
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2025-07-30 01:18:51 +0200

    Add melos setup and check only set up Dart for now

--- dart/lib/src/exception_cause.dart
+++
@@ -1,8 +0,0 @@
-/// Holds inner exception and stackTrace combinations contained in other exceptions
-class ExceptionCause {
-  ExceptionCause(this.exception, this.stackTrace, {this.source});
-
-  dynamic exception;
-  dynamic stackTrace;
-  String? source;
-}
--- dart/lib/src/http_client/sentry_http_client_error.dart
+++
@@ -1,7 +0,0 @@
-class SentryHttpClientError implements Exception {
-  final String _message;
-  SentryHttpClientError(this._message);
-
-  @override
-  String toString() => 'Exception: $_message';
-}
