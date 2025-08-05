476b8ed85306b6e93facc15d82a99e82c2283b1e
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2025-08-05 13:41:16 +0200

    Fix test

--- packages/drift/test/utils/windows_helper.dart
+++ packages/drift/test/utils/windows_helper.dart
@@ -1,8 +1,14 @@
 import 'dart:ffi';
-import 'dart:io';
 
 DynamicLibrary openOnWindows() {
-  final scriptDir = File(Platform.script.toFilePath()).parent;
-  final libraryNextToScript = File('${scriptDir.path}/test/sqlite3.dll');
-  return DynamicLibrary.open(libraryNextToScript.path);
+  // Use the system SQLite DLL installed by Chocolatey
+  try {
+    // Try to load from system path first
+    return DynamicLibrary.open('sqlite3.dll');
+  } catch (e) {
+    // Fallback to absolute path from Chocolatey installation
+    return DynamicLibrary.open(
+      'C:\\ProgramData\\chocolatey\\lib\\sqlite\\tools\\sqlite3.dll',
+    );
+  }
 }
