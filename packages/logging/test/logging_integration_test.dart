d6d541b5a8ea759547afec642ad17bd6a58230d0
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2025-08-07 15:34:27 +0200

    Update

--- packages/logging/test/logging_integration_test.dart
+++ packages/logging/test/logging_integration_test.dart
@@ -330,7 +330,8 @@ void main() {
       expect(basicAttributes['loggerName']?.value, 'TestLogger');
       expect(basicAttributes['sequenceNumber']?.value, isA<int>());
       expect(basicAttributes['time']?.value, isA<int>());
-      expect(basicAttributes['sentry.origin']?.value, LoggingIntegration.origin);
+      expect(
+          basicAttributes['sentry.origin']?.value, LoggingIntegration.origin);
       expect(basicAttributes.containsKey('error'), false);
       expect(basicAttributes.containsKey('stackTrace'), false);
 
