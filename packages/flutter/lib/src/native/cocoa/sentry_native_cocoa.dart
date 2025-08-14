0863972ab9571c1d9d398e62954ee06fab988936
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2025-08-14 21:36:43 +0200

    Fix imports

--- packages/flutter/lib/src/native/cocoa/sentry_native_cocoa.dart
+++ packages/flutter/lib/src/native/cocoa/sentry_native_cocoa.dart
@@ -1,4 +1,7 @@
 import 'dart:async';
+import 'dart:ffi';
+import 'dart:typed_data';
+import 'package:ffi/ffi.dart';
 import 'package:meta/meta.dart';
 import 'package:objective_c/objective_c.dart';
 
