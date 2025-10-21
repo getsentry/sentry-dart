5ef5a4ab1ea4eb9b24037e9ccb76ad06d8ed9618
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2025-10-21 12:59:33 +0200

    Update

--- packages/flutter/lib/src/native/java/sentry_native_java.dart
+++ packages/flutter/lib/src/native/java/sentry_native_java.dart
@@ -247,23 +247,28 @@ class SentryNativeJava extends SentryNativeChannel {
       });
 
   @override
-  void setUser(SentryUser? user) => tryCatchSync('setUser', () {
-        native.User? nativeUser;
-        JObject? nativeOptions;
+  void setUser(SentryUser? user) {
+    native.User? nativeUser;
+    JObject? nativeOptions;
 
-        if (user == null) {
-          native.Sentry.setUser(null);
-        } else {
-          nativeOptions = native.ScopesAdapter.getInstance()?.getOptions();
-          if (nativeOptions == null) return;
+    tryCatchSync('setUser', () {
+      if (user == null) {
+        native.Sentry.setUser(null);
+      } else {
+        nativeOptions = native.ScopesAdapter.getInstance()?.getOptions();
+        if (nativeOptions == null) return;
 
-          nativeUser =
-              native.User.fromMap(_dartToJMap(user.toJson()), nativeOptions);
-          if (nativeUser == null) return;
+        nativeUser =
+            native.User.fromMap(_dartToJMap(user.toJson()), nativeOptions!);
+        if (nativeUser == null) return;
 
-          native.Sentry.setUser(nativeUser);
-        }
-      });
+        native.Sentry.setUser(nativeUser);
+      }
+    }, finallyFn: () {
+      nativeUser?.release();
+      nativeOptions?.release();
+    });
+  }
 }
 
 JObject? _dartToJObject(Object? value) => switch (value) {
