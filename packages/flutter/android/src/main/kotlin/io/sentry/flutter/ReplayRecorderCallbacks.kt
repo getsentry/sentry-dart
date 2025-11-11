a220e61b8fb864a7c985b2d21a4f493999271307
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2025-11-11 15:49:41 +0100

    Ktlint

--- packages/flutter/android/src/main/kotlin/io/sentry/flutter/ReplayRecorderCallbacks.kt
+++ packages/flutter/android/src/main/kotlin/io/sentry/flutter/ReplayRecorderCallbacks.kt
@@ -1,10 +1,18 @@
 package io.sentry.flutter
 
 interface ReplayRecorderCallbacks {
-  fun replayStarted(replayId: String, replayIsBuffering: Boolean)
+  fun replayStarted(
+    replayId: String,
+    replayIsBuffering: Boolean
+  )
+
   fun replayResumed()
+
   fun replayPaused()
+
   fun replayStopped()
+
   fun replayReset()
+
   fun replayConfigChanged(width: Int, height: Int, frameRate: Int)
 }
