b84cec3fed6183ac7ead9c715ac045f7f00debb0
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2025-11-26 17:35:50 +0100

    Update

--- packages/dart/test/hub_test.dart
+++ packages/dart/test/hub_test.dart
@@ -3,7 +3,6 @@ import 'package:mockito/mockito.dart';
 import 'package:sentry/sentry.dart';
 import 'package:sentry/src/client_reports/discard_reason.dart';
 import 'package:sentry/src/propagation_context.dart';
-import 'package:sentry/src/protocol/unset_span.dart';
 import 'package:sentry/src/sentry_tracer.dart';
 import 'package:sentry/src/transport/data_category.dart';
 import 'package:test/test.dart';
