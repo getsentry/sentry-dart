735ff6e9c99bdf2b216962ff09e526cdd382ec7f
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2025-12-09 23:09:25 +0100

    Rename native FFI JNI utility test file to native JNI utility test for clarity

--- packages/flutter/example/integration_test/all.dart
+++ packages/flutter/example/integration_test/all.dart
@@ -3,7 +3,7 @@ import 'integration_test.dart' as a;
 import 'profiling_test.dart' as b;
 import 'replay_test.dart' as c;
 import 'platform_integrations_test.dart' as d;
-import 'native_ffi_jni_utils_test.dart' as e;
+import 'native_jni_utils_test.dart' as e;
 
 void main() {
   a.main();
