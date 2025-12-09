d3251d0cfe8c3780b0e6e0f05f400297d2123bc4
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2025-12-09 23:04:06 +0100

    Add JNI utility tests for Dart to Java object conversion
    This commit introduces a new test file for verifying the conversion of Dart objects to JNI types, including primitives, lists, and maps. The tests ensure that null values are dropped appropriately and that nested structures are handled correctly. The tests are designed to run on the Android platform only.

--- packages/flutter/example/integration_test/native_jni_utils_test.dart
+++ packages/flutter/example/integration_test/native_jni_utils_test.dart
@@ -6,8 +6,6 @@ import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:jni/jni.dart';
-import 'package:objective_c/objective_c.dart';
-import 'package:sentry_flutter/src/native/cocoa/sentry_native_cocoa.dart';
 import 'package:sentry_flutter/src/native/java/sentry_native_java.dart';
 
 import 'utils.dart';
@@ -81,34 +79,6 @@ void main() {
       _verifyJniMap(jMap);
     });
   }, skip: !Platform.isAndroid);
-
-  group('FFI (iOS/macOS)', () {
-    test('dartToNSObject converts primitives', () {
-      _expectNSString(dartToNSObject('value'), 'value');
-      _expectNSInt(dartToNSObject(1), 1);
-      _expectNSDouble(dartToNSObject(1.1), 1.1);
-      _expectNSBool(dartToNSObject(true), true);
-      _expectNSString(dartToNSObject(_customObject), _customObject.toString());
-    });
-
-    test('dartToNSObject converts list (drops nulls)', () {
-      final nsArray = NSArray.castFrom(dartToNSObject(_testList));
-      _verifyNSArray(nsArray);
-    });
-
-    test('dartToNSObject converts map (drops null values)', () {
-      final nsDict = NSDictionary.castFrom(dartToNSObject(_testMap));
-      _verifyNSDictionary(nsDict);
-    });
-
-    test('dartToNSArray', () {
-      _verifyNSArray(dartToNSArray(_testList));
-    });
-
-    test('dartToNSDictionary', () {
-      _verifyNSDictionary(dartToNSDictionary(_testMap));
-    });
-  }, skip: !(Platform.isIOS || Platform.isMacOS));
 }
 
 void _expectJniString(JObject obj, String expected) {
@@ -201,83 +171,4 @@ void _verifyJniNestedMap(JMap<JString, JObject> map) {
 
   // Verify null was dropped
   expect(_jniIsNull(_jniGet(map, 'innerNull')), isTrue);
-}
-
-void _expectNSString(ObjCObjectBase obj, String expected) {
-  expect(NSString.isInstance(obj), isTrue);
-  expect(NSString.castFrom(obj).toDartString(), expected);
-}
-
-void _expectNSInt(ObjCObjectBase obj, int expected) {
-  expect(NSNumber.isInstance(obj), isTrue);
-  expect(NSNumber.castFrom(obj).longLongValue, expected);
-}
-
-void _expectNSDouble(ObjCObjectBase obj, double expected) {
-  expect(NSNumber.isInstance(obj), isTrue);
-  expect(NSNumber.castFrom(obj).doubleValue, expected);
-}
-
-void _expectNSBool(ObjCObjectBase obj, bool expected) {
-  expect(NSNumber.isInstance(obj), isTrue);
-  expect(NSNumber.castFrom(obj).boolValue, expected);
-}
-
-ObjCObjectBase? _nsGet(NSDictionary dict, String key) =>
-    dict.objectForKey(key.toNSString());
-
-void _verifyNSArray(NSArray array) {
-  expect(array.count, _expectedListLength);
-
-  // Verify primitives
-  expect(NSString.castFrom(array.objectAtIndex(0)).toDartString(), 'value');
-  expect(NSNumber.castFrom(array.objectAtIndex(1)).longLongValue, 1);
-  expect(NSNumber.castFrom(array.objectAtIndex(2)).doubleValue, 1.1);
-  expect(NSNumber.castFrom(array.objectAtIndex(3)).boolValue, isTrue);
-  expect(NSString.castFrom(array.objectAtIndex(4)).toDartString(),
-      _customObject.toString());
-
-  // Verify nested list
-  final nestedList = NSArray.castFrom(array.objectAtIndex(5));
-  expect(nestedList.count, 2);
-  expect(NSString.castFrom(nestedList.objectAtIndex(0)).toDartString(),
-      'nestedList');
-  expect(NSNumber.castFrom(nestedList.objectAtIndex(1)).longLongValue, 2);
-
-  // Verify nested map
-  _verifyNSNestedDict(NSDictionary.castFrom(array.objectAtIndex(6)));
-}
-
-void _verifyNSDictionary(NSDictionary dict) {
-  expect(dict.count, _expectedMapLength);
-
-  // Verify primitives
-  expect(NSString.castFrom(_nsGet(dict, 'key')!).toDartString(), 'value');
-  expect(NSNumber.castFrom(_nsGet(dict, 'key2')!).longLongValue, 1);
-  expect(NSNumber.castFrom(_nsGet(dict, 'key3')!).doubleValue, 1.1);
-  expect(NSNumber.castFrom(_nsGet(dict, 'key4')!).boolValue, isTrue);
-  expect(NSString.castFrom(_nsGet(dict, 'key5')!).toDartString(),
-      _customObject.toString());
-
-  // Verify nested list
-  _verifyNSArray(NSArray.castFrom(_nsGet(dict, 'list')!));
-
-  // Verify nested map
-  _verifyNSNestedDict(NSDictionary.castFrom(_nsGet(dict, 'nestedMap')!));
-
-  // Verify null was dropped
-  expect(_nsGet(dict, 'nullEntry'), isNull);
-}
-
-void _verifyNSNestedDict(NSDictionary dict) {
-  expect(
-      NSString.castFrom(_nsGet(dict, 'innerString')!).toDartString(), 'nested');
-
-  final innerList = NSArray.castFrom(_nsGet(dict, 'innerList')!);
-  expect(innerList.count, _expectedNestedListLength);
-  expect(NSNumber.castFrom(innerList.objectAtIndex(0)).longLongValue, 1);
-  expect(NSNumber.castFrom(innerList.objectAtIndex(1)).longLongValue, 2);
-
-  // Verify null was dropped
-  expect(_nsGet(dict, 'innerNull'), isNull);
 }
