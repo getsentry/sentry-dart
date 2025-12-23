0ffd9fb0e8db64c17067126e23815caaa0bef3e8
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2025-12-23 11:22:41 +0100

    refactor(sentry): remove unused debug logger import from sentry_options.dart
    This commit removes the unused import of the debug logger from the `sentry_options.dart` file, contributing to cleaner code and improved maintainability.

--- packages/dart/lib/src/sentry_options.dart
+++ packages/dart/lib/src/sentry_options.dart
@@ -13,7 +13,6 @@ import 'platform/platform.dart';
 import 'sentry_exception_factory.dart';
 import 'sentry_stack_trace_factory.dart';
 import 'transport/noop_transport.dart';
-import 'utils/debug_logger.dart';
 import 'version.dart';
 import 'sentry_log_batcher.dart';
 import 'noop_log_batcher.dart';
