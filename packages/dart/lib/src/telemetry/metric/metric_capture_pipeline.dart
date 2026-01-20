c4221aa2866c38476482bf22ccb412fdbb092ddb
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2026-01-20 11:57:10 +0100

    Update

--- packages/dart/lib/src/telemetry/metric/metric_capture_pipeline.dart
+++ packages/dart/lib/src/telemetry/metric/metric_capture_pipeline.dart
@@ -20,42 +20,54 @@ class MetricCapturePipeline {
       return;
     }
 
-    if (scope != null) {
-      metric.attributes.addAllIfAbsent(scope.attributes);
-    }
+    try {
+      if (scope != null) {
+        metric.attributes.addAllIfAbsent(scope.attributes);
+      }
 
-    await _options.lifecycleRegistry
-        .dispatchCallback<OnProcessMetric>(OnProcessMetric(metric));
+      await _options.lifecycleRegistry
+          .dispatchCallback<OnProcessMetric>(OnProcessMetric(metric));
 
-    metric.attributes.addAllIfAbsent(defaultAttributes(_options, scope: scope));
+      metric.attributes
+          .addAllIfAbsent(defaultAttributes(_options, scope: scope));
 
-    final beforeSendMetric = _options.beforeSendMetric;
-    SentryMetric? processedMetric = metric;
-    if (beforeSendMetric != null) {
-      try {
-        processedMetric = await beforeSendMetric(metric);
-      } catch (exception, stackTrace) {
-        _options.log(
-          SentryLevel.error,
-          'The beforeSendMetric callback threw an exception',
-          exception: exception,
-          stackTrace: stackTrace,
-        );
-        if (_options.automatedTestMode) {
-          rethrow;
+      final beforeSendMetric = _options.beforeSendMetric;
+      SentryMetric? processedMetric = metric;
+      if (beforeSendMetric != null) {
+        try {
+          processedMetric = await beforeSendMetric(metric);
+        } catch (exception, stackTrace) {
+          _options.log(
+            SentryLevel.error,
+            'The beforeSendMetric callback threw an exception',
+            exception: exception,
+            stackTrace: stackTrace,
+          );
+          if (_options.automatedTestMode) {
+            rethrow;
+          }
         }
       }
-    }
-    if (processedMetric == null) {
-      _options.recorder
-          .recordLostEvent(DiscardReason.beforeSend, DataCategory.metric);
-      internalLogger.debug(
-          '$MetricCapturePipeline: Metric ${metric.name} dropped by beforeSendMetric');
-      return;
-    }
+      if (processedMetric == null) {
+        _options.recorder
+            .recordLostEvent(DiscardReason.beforeSend, DataCategory.metric);
+        internalLogger.debug(
+            '$MetricCapturePipeline: Metric ${metric.name} dropped by beforeSendMetric');
+        return;
+      }
 
-    _options.telemetryProcessor.addMetric(processedMetric);
-    internalLogger.debug(
-        '$MetricCapturePipeline: Metric ${processedMetric.name} (${processedMetric.type}) captured');
+      _options.telemetryProcessor.addMetric(processedMetric);
+      internalLogger.debug(
+          '$MetricCapturePipeline: Metric ${processedMetric.name} (${processedMetric.type}) captured');
+    } catch (exception, stackTrace) {
+      internalLogger.error(
+        'Error capturing metric ${metric.name}',
+        error: exception,
+        stackTrace: stackTrace,
+      );
+      if (_options.automatedTestMode) {
+        rethrow;
+      }
+    }
   }
 }
