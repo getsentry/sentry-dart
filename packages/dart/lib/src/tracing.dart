012c878eba5df1f3f7205835d3bf72a55721499b
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2026-01-27 17:13:14 +0100

    Update

--- packages/dart/lib/src/tracing.dart
+++ packages/dart/lib/src/tracing.dart
@@ -8,9 +8,11 @@ export 'sentry_measurement.dart';
 export 'sentry_measurement_unit.dart';
 export 'sentry_trace_context_header.dart';
 export 'sentry_traces_sampling_decision.dart';
+// ignore: invalid_export_of_internal_element
 export 'utils/tracing/span_wrapper.dart';
 // ignore: invalid_export_of_internal_element
 export 'utils/tracing/legacy_span_wrapper.dart';
+// ignore: invalid_export_of_internal_element
 export 'utils/tracing/transaction_span_wrapper.dart';
 // ignore: invalid_export_of_internal_element
 export 'utils/tracing/legacy_transaction_span_wrapper.dart';
