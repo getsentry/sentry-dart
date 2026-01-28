abff1b6f500cd43f6ab82e5a77738f0057c69085
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2026-01-28 15:40:25 +0100

    Update

--- packages/drift/lib/src/internal_logger.dart
+++ packages/drift/lib/src/internal_logger.dart
@@ -1,6 +1,5 @@
 import 'package:meta/meta.dart';
 import 'package:sentry/sentry.dart';
 
-/// Internal logger for sentry_drift package.
 @internal
 const internalLogger = SentryInternalLogger('sentry_drift');
--- packages/hive/lib/src/internal_logger.dart
+++ packages/hive/lib/src/internal_logger.dart
@@ -1,6 +1,5 @@
 import 'package:meta/meta.dart';
 import 'package:sentry/sentry.dart';
 
-/// Internal logger for sentry_hive package.
 @internal
 const internalLogger = SentryInternalLogger('sentry_hive');
