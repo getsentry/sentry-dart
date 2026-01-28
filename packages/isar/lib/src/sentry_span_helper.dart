ee0ec708049e439ce5a3131bcb7e01ceedd8b824
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2026-01-28 16:43:41 +0100

    Update

--- packages/isar/lib/src/sentry_span_helper.dart
+++ packages/isar/lib/src/sentry_span_helper.dart
@@ -46,6 +46,7 @@ class SentrySpanHelper {
     final breadcrumb = Breadcrumb(
       message: description,
       data: {
+        SentryIsar.dbSystemKey: SentryIsar.dbSystem,
         if (dbName != null) SentryIsar.dbNameKey: dbName,
         if (collectionName != null) SentryIsar.dbCollectionKey: collectionName,
       },
@@ -97,6 +98,7 @@ class SentrySpanHelper {
     final breadcrumb = Breadcrumb(
       message: description,
       data: {
+        SentryIsar.dbSystemKey: SentryIsar.dbSystem,
         if (dbName != null) SentryIsar.dbNameKey: dbName,
         if (collectionName != null) SentryIsar.dbCollectionKey: collectionName,
       },
