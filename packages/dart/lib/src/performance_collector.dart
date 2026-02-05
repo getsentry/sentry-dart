2c27e381e33ffa3fc1350a2555cc2010de63796b
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2026-02-05 13:32:11 +0100

    Update

--- packages/dart/lib/src/performance_collector.dart
+++ packages/dart/lib/src/performance_collector.dart
@@ -4,28 +4,10 @@ abstract class PerformanceCollector {}
 
 /// Used for collecting continuous data about vitals (slow, frozen frames, etc.)
 /// during a transaction/span.
-///
-/// Deprecated: This interface is being replaced by collectors that work with
-/// [InstrumentationSpan] for unified span handling.
-@Deprecated('Use collectors that work with InstrumentationSpan instead')
 abstract class PerformanceContinuousCollector extends PerformanceCollector {
   Future<void> onSpanStarted(ISentrySpan span);
 
   Future<void> onSpanFinished(ISentrySpan span, DateTime endTimestamp);
-
-  void clear();
-}
-
-/// Used for collecting continuous data about vitals (slow, frozen frames, etc.)
-/// during a span (v2 API).
-///
-/// Deprecated: This interface is being replaced by collectors that work with
-/// [InstrumentationSpan] for unified span handling.
-@Deprecated('Use collectors that work with InstrumentationSpan instead')
-abstract class PerformanceContinuousCollectorV2 extends PerformanceCollector {
-  Future<void> onSpanStarted(SentrySpanV2 span);
-
-  Future<void> onSpanFinished(SentrySpanV2 span, DateTime endTimestamp);
 
   void clear();
 }
