39a1e00b9a5deb83b39765c425f60e31761f011d
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2026-02-12 12:57:53 +0100

    Updat

--- packages/flutter/test/native/sentry_native_java_test.dart
+++ packages/flutter/test/native/sentry_native_java_test.dart
@@ -1,2 +1,10 @@
-export 'sentry_native_java_test_real.dart'
-    if (dart.library.js_interop) 'sentry_native_java_test_web.dart';
+@TestOn('vm')
+library;
+
+import 'package:flutter_test/flutter_test.dart';
+
+// ignore: unused_import
+import 'sentry_native_java_test_web.dart'
+    if (dart.library.io) 'sentry_native_java_test_real.dart' as actual;
+
+void main() => actual.main();
