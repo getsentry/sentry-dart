1275bad8127b65b6b4c93dfe41633fb2effc8226
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2026-02-19 16:11:36 +0100

    Update test

--- packages/dart/test/telemetry/span/ignore_span_rule_test.dart
+++ packages/dart/test/telemetry/span/ignore_span_rule_test.dart
@@ -8,28 +8,28 @@ void main() {
         test('matches when name contains substring', () {
           final rule = IgnoreSpanRule.nameContains('http');
 
-          expect(rule.nameMatches('http.client'), isTrue);
-          expect(rule.nameMatches('my-http-request'), isTrue);
-          expect(rule.nameMatches('send-http'), isTrue);
+          expect(rule.appliesToName('http.client'), isTrue);
+          expect(rule.appliesToName('my-http-request'), isTrue);
+          expect(rule.appliesToName('send-http'), isTrue);
         });
 
         test('does not match when name lacks substring', () {
           final rule = IgnoreSpanRule.nameContains('http');
 
-          expect(rule.nameMatches('db.query'), isFalse);
-          expect(rule.nameMatches('file.read'), isFalse);
+          expect(rule.appliesToName('db.query'), isFalse);
+          expect(rule.appliesToName('file.read'), isFalse);
         });
 
         test('matches exact name', () {
           final rule = IgnoreSpanRule.nameContains('http');
 
-          expect(rule.nameMatches('http'), isTrue);
+          expect(rule.appliesToName('http'), isTrue);
         });
 
         test('does not match empty name', () {
           final rule = IgnoreSpanRule.nameContains('http');
 
-          expect(rule.nameMatches(''), isFalse);
+          expect(rule.appliesToName(''), isFalse);
         });
       });
 
@@ -37,15 +37,15 @@ void main() {
         test('matches when name contains regex match', () {
           final rule = IgnoreSpanRule.nameContains(RegExp(r'http\.\w+'));
 
-          expect(rule.nameMatches('http.client'), isTrue);
-          expect(rule.nameMatches('prefix-http.get-suffix'), isTrue);
+          expect(rule.appliesToName('http.client'), isTrue);
+          expect(rule.appliesToName('prefix-http.get-suffix'), isTrue);
         });
 
         test('does not match when regex does not match', () {
           final rule = IgnoreSpanRule.nameContains(RegExp(r'http\.\w+'));
 
-          expect(rule.nameMatches('http.'), isFalse);
-          expect(rule.nameMatches('db.query'), isFalse);
+          expect(rule.appliesToName('http.'), isFalse);
+          expect(rule.appliesToName('db.query'), isFalse);
         });
       });
     });
@@ -54,34 +54,34 @@ void main() {
       test('matches when name equals value exactly', () {
         final rule = IgnoreSpanRule.nameEquals('http.client');
 
-        expect(rule.nameMatches('http.client'), isTrue);
+        expect(rule.appliesToName('http.client'), isTrue);
       });
 
       test('does not match partial names', () {
         final rule = IgnoreSpanRule.nameEquals('http.client');
 
-        expect(rule.nameMatches('http.client.get'), isFalse);
-        expect(rule.nameMatches('http'), isFalse);
-        expect(rule.nameMatches('my-http.client'), isFalse);
+        expect(rule.appliesToName('http.client.get'), isFalse);
+        expect(rule.appliesToName('http'), isFalse);
+        expect(rule.appliesToName('my-http.client'), isFalse);
       });
 
       test('is case-sensitive', () {
         final rule = IgnoreSpanRule.nameEquals('HTTP.client');
 
-        expect(rule.nameMatches('http.client'), isFalse);
-        expect(rule.nameMatches('HTTP.client'), isTrue);
+        expect(rule.appliesToName('http.client'), isFalse);
+        expect(rule.appliesToName('HTTP.client'), isTrue);
       });
 
       test('does not match empty name', () {
         final rule = IgnoreSpanRule.nameEquals('http.client');
 
-        expect(rule.nameMatches(''), isFalse);
+        expect(rule.appliesToName(''), isFalse);
       });
 
       test('matches empty value against empty name', () {
         final rule = IgnoreSpanRule.nameEquals('');
 
-        expect(rule.nameMatches(''), isTrue);
+        expect(rule.appliesToName(''), isTrue);
       });
     });
 
@@ -90,16 +90,16 @@ void main() {
         test('matches when name starts with prefix', () {
           final rule = IgnoreSpanRule.nameStartsWith('http');
 
-          expect(rule.nameMatches('http.client'), isTrue);
-          expect(rule.nameMatches('http'), isTrue);
-          expect(rule.nameMatches('http.get'), isTrue);
+          expect(rule.appliesToName('http.client'), isTrue);
+          expect(rule.appliesToName('http'), isTrue);
+          expect(rule.appliesToName('http.get'), isTrue);
         });
 
         test('does not match when name does not start with prefix', () {
           final rule = IgnoreSpanRule.nameStartsWith('http');
 
-          expect(rule.nameMatches('my-http'), isFalse);
-          expect(rule.nameMatches('db.query'), isFalse);
+          expect(rule.appliesToName('my-http'), isFalse);
+          expect(rule.appliesToName('db.query'), isFalse);
         });
       });
 
@@ -107,14 +107,14 @@ void main() {
         test('matches when name starts with regex match', () {
           final rule = IgnoreSpanRule.nameStartsWith(RegExp(r'(http|grpc)\.'));
 
-          expect(rule.nameMatches('http.client'), isTrue);
-          expect(rule.nameMatches('grpc.call'), isTrue);
+          expect(rule.appliesToName('http.client'), isTrue);
+          expect(rule.appliesToName('grpc.call'), isTrue);
         });
 
         test('does not match when regex matches later in name', () {
           final rule = IgnoreSpanRule.nameStartsWith(RegExp(r'http\.'));
 
-          expect(rule.nameMatches('prefix-http.client'), isFalse);
+          expect(rule.appliesToName('prefix-http.client'), isFalse);
         });
       });
     });
@@ -123,28 +123,28 @@ void main() {
       test('matches when name ends with suffix', () {
         final rule = IgnoreSpanRule.nameEndsWith('.query');
 
-        expect(rule.nameMatches('db.query'), isTrue);
-        expect(rule.nameMatches('sql.query'), isTrue);
-        expect(rule.nameMatches('.query'), isTrue);
+        expect(rule.appliesToName('db.query'), isTrue);
+        expect(rule.appliesToName('sql.query'), isTrue);
+        expect(rule.appliesToName('.query'), isTrue);
       });
 
       test('does not match when name does not end with suffix', () {
         final rule = IgnoreSpanRule.nameEndsWith('.query');
 
-        expect(rule.nameMatches('db.query.execute'), isFalse);
-        expect(rule.nameMatches('query'), isFalse);
+        expect(rule.appliesToName('db.query.execute'), isFalse);
+        expect(rule.appliesToName('query'), isFalse);
       });
 
       test('matches exact name equal to suffix', () {
         final rule = IgnoreSpanRule.nameEndsWith('query');
 
-        expect(rule.nameMatches('query'), isTrue);
+        expect(rule.appliesToName('query'), isTrue);
       });
 
       test('does not match empty name', () {
         final rule = IgnoreSpanRule.nameEndsWith('.query');
 
-        expect(rule.nameMatches(''), isFalse);
+        expect(rule.appliesToName(''), isFalse);
       });
     });
   });
