286cad79ca7c1170d75c1a415846553476c9b0d3
buenaflor <23364143+buenaflor@users.noreply.github.com>
2026-09-17 14:05:15 +0000

    release: 10.0.0-alpha.6

--- packages/dart/lib/src/version.dart
+++ packages/dart/lib/src/version.dart
@@ -9,7 +9,7 @@
 library;
 
 /// The SDK version reported to Sentry.io in the submitted events.
-const String sdkVersion = '10.0.0-alpha.5';
+const String sdkVersion = '10.0.0-alpha.6';
 
 String sdkName(bool isWeb) => isWeb ? _browserSdkName : _ioSdkName;
 
